ba56e264b5db6c17ab718dd67a6219a533692cb4
b2blosangeles <b2blosangeles@gmail.com>
2017-10-20 14:41:54 -0700

    Update cron_whoami.js

--- cron_service/cron_whoami.js
+++ cron_service/cron_whoami.js
@@ -2,7 +2,8 @@ var path = require('path');
 var env = {root_path:path.join(__dirname, '../..')};
 env.site_path = env.root_path + '/site';
 var request =  require(env.root_path + '/package/request/node_modules/request');
-
+var diskspace = require(env.root_path + '/package/diskspace/node_modules/diskspace'),
+    
 function getServerIP() {
     var ifaces = require('os').networkInterfaces(), address=[];
     for (var dev in ifaces) {
@@ -11,14 +12,18 @@ function getServerIP() {
     }
     return address;
 };
-request({
-  url: 'http://root.qalet.com/api/whoami.js',
-  headers: {
-    "content-type": "application/json"
-  },
-  form:{ip:getServerIP()}
-}, function (error, resp, body) { 
-        var s = {};
-        try { s = JSON.parse(body); } catch (e) {}
-        process.stdout.write(s.value);
+
+diskspace.check('/', function (err, space)
+{
+    request({
+      url: 'http://root.qalet.com/api/whoami.js',
+      headers: {
+        "content-type": "application/json"
+      },
+      form:{ip:getServerIP(), space:space}
+    }, function (error, resp, body) { 
+            var s = {};
+            try { s = JSON.parse(body); } catch (e) {}
+            process.stdout.write(s.value);
+    });
 });
