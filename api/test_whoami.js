792964f0b10c70bf04cb7332a3953cdb4c617723
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 22:04:24 -0700

    Create test_whoami.js

---
+++ api/test_whoami.js
@@ -0,0 +1,8 @@
+pkg.request({
+  url: 'http://root.qalet.com/api/whoami.js',
+  headers: {
+      "content-type": "application/json"
+  }
+    }, function (error, resp, body) { 
+       res.send('NIU');
+   });
