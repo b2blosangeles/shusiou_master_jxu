01fe765adb1b58fe722189b4037132aba7a6d8ee
b2blosangeles <b2blosangeles@gmail.com>
2017-10-20 06:25:28 -0700

    Create who.js

---
+++ api/who.js
@@ -0,0 +1,9 @@
+function getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};
+res.send(env);
