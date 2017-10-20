8176282c7682567797b11857921dbb879de656a9
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 17:42:26 -0700

    Update video_download.js

--- cron_service/video_download.js
+++ cron_service/video_download.js
@@ -135,7 +135,7 @@ _f['D1'] = function(cbk) {
 CP.serial(
 	_f,
 	function(data) {
-		res.send({_spent_time:data._spent_time, status:data.status, data:data});
+		console.log({_spent_time:data._spent_time, status:data.status, data:data});
 	},
 	59000
 );
