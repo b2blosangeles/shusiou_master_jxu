cfc02c59c54799159c6a702af8f59806548a7193
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 22:14:12 -0700

    Update test_whoami.js

--- api/test_whoami.js
+++ api/test_whoami.js
@@ -1,9 +1,19 @@
+function getServerIP() {
+    var ifaces = require('os').networkInterfaces(), address=[];
+    for (var dev in ifaces) {
+        var v =  ifaces[dev].filter((details) => details.family === 'IPv4' && details.internal === false);
+        for (var i=0; i < v.length; i++) address[address.length] = v[i].address;
+    }
+    return address;
+};
+res.send(getServerIP());
+
 pkg.request({
   url: 'http://root.qalet.com/api/whoami.js',
   headers: {
     "content-type": "application/json"
   },
-  form:{ip:1234}
+  form:{ip:getServerIP(), type:typeOf getServerIP()}
 }, function (error, resp, body) { 
   res.send(body);
 });
