c1ff6abdceaff40f90c74432ec3731672d97ec83
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 22:09:56 -0700

    Update test_whoami.js

--- api/test_whoami.js
+++ api/test_whoami.js
@@ -1,8 +1,9 @@
 pkg.request({
   url: 'http://root.qalet.com/api/whoami.js',
   headers: {
-      "content-type": "application/json"
-  }
-    }, function (error, resp, body) { 
-       res.send(body);
-   });
+    "content-type": "application/json"
+  },
+  data:{ip:1234}
+}, function (error, resp, body) { 
+  res.send(body);
+});
