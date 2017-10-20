cb4b5416f71b7b48054513cb360cc756a021d434
b2blosangeles <b2blosangeles@gmail.com>
2017-10-19 19:33:15 -0700

    Update git_service.js

--- cron_service/git_service.js
+++ cron_service/git_service.js
@@ -1,5 +1,5 @@
 var exec = require('child_process').exec;
-process.stdout.write("hello: ");
+process.stdout.write({f:'hello'});
 var path = require('path');
 var env = {root_path:path.join(__dirname, '../..')};
 
