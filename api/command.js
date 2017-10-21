27b12a9f910d4e4c62f0b6efbdf1ea691b80c711
b2blosangeles <b2blosangeles@gmail.com>
2017-10-21 06:56:54 -0700

    Update command.js

--- api/command.js
+++ api/command.js
@@ -2,6 +2,13 @@ if (!req.query['opt']) {
 	res.send('Missing opt parpmeter error');
 	return true;
 }
-pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
-	 res.send(stdout);
-});
+switch(req.query['opt']) {
+    case 'git_frame_pull':
+	pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
+		 res.send(stdout);
+	});
+        break;
+    default:
+	res.send('Wrong opt parpmeter!!');
+	return true;
+}
