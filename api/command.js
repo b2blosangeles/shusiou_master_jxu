6ef143a6e7a160af6656117b6ad7097aab2b9037
b2blosangeles <b2blosangeles@gmail.com>
2017-10-21 06:57:20 -0700

    Update command.js

--- api/command.js
+++ api/command.js
@@ -8,6 +8,11 @@ switch(req.query['opt']) {
 		 res.send(stdout);
 	});
         break;
+    case 'git_space_pull':
+	pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
+		 res.send(stdout);
+	});
+        break;		
     default:
 	res.send('Wrong opt parpmeter!!');
 	return true;
