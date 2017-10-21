a48772fac36723bad9de91f33ad0a53864a7930b
b2blosangeles <b2blosangeles@gmail.com>
2017-10-21 06:54:50 -0700

    Update command.js

--- api/command.js
+++ api/command.js
@@ -2,3 +2,6 @@ if (!req.query['opt']) {
 	res.send('Missing opt parpmeter error');
 	return true;
 }
+pkg.exec('cd ' + env.root_path + '&& git pull', function(error, stdout, stderr) {
+	 res.send(stdout);
+});
