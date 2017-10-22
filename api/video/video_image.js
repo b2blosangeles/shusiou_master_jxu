ca087455422c2007f823ee15a0879140becec9ef
b2blosangeles <b2blosangeles@gmail.com>
2017-10-22 16:46:38 -0700

    Update video_image.js

--- api/video/video_image.js
+++ api/video/video_image.js
@@ -1,8 +1,8 @@
 var video_folder = '/var/video/';
-var vid = 1;
+var vid = 2;
 file_video =  video_folder + vid + '.mp4';
 
-var folder_image = '/tmp/images/ '+ vid;
+var folder_image = '/tmp/images/ '+ vid + '.mp4';
 
 if (!req.query['s']) {
 	res.send('S error');
