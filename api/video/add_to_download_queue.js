5a2a957ab055472f228b996911786ed5c0c8091b
b2blosangeles <b2blosangeles@gmail.com>
2017-10-22 08:28:45 -0700

    Update add_to_download_queue.js

--- api/video/add_to_download_queue.js
+++ api/video/add_to_download_queue.js
@@ -25,7 +25,7 @@ _f['P1'] = function(cbk) {
 	
 	var str = 'INSERT INTO `download_queue` (`source`, `code`, `uid`, `info`,`created`, `status`) ' +
 				'values ("' + source + '", "' + encodeURIComponent(code) + '", "' + uid + '", ' +
-	    			"'" + 'encodeURIComponent(JSON.stringify(CP.data.P0))'  + "'" + 
+	    		//	"'" + 'encodeURIComponent(JSON.stringify(CP.data.P0))'  + "'" + 
 				'NOW(), 0 ); ';
 	cbk(str);
 	return true
