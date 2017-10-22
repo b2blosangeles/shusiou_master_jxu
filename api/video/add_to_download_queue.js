a9f9bcdad122950627b1eacd20ef6dfcf76c6324
b2blosangeles <b2blosangeles@gmail.com>
2017-10-22 08:18:03 -0700

    Update add_to_download_queue.js

--- api/video/add_to_download_queue.js
+++ api/video/add_to_download_queue.js
@@ -20,13 +20,15 @@ _f['P0'] = function(cbk) {
 };
 _f['P1'] = function(cbk) {
 	var cfg0 = require(env.root_path + '/api/cfg/db.json');
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
+//	var connection = mysql.createConnection(cfg0);
+//	connection.connect();
 	
 	var str = 'INSERT INTO `download_queue` (`source`, `code`, `uid`, `info`,`created`, `status`) ' +
 				'values ("' + source + '", "' + encodeURIComponent(code) + '", "' + uid + '", ' +
 	    			"'" + encodeURIComponent(JSON.stringify(CP.data.P0))  + "'" + 
 				'NOW(), 0 ); ';
+	cbk(str);
+	return true
 	connection.query(str, function (error, results, fields) {
 		connection.end();
 		if (error) {
