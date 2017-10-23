d4be4110ada895d75ac13b27f273a200d444bd11
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 08:39:11 -0700

    Update docview_page.jsx

--- files/mservices/react_module/lang_space/view/docview_page.jsx
+++ files/mservices/react_module/lang_space/view/docview_page.jsx
@@ -21,8 +21,7 @@ try {
 		},
 		loadDocument:function(code, lang) {
 			var me = this;
-			var url = '/api/doc/viewdoc.api';
-			alert(url);
+			var url = 'http://shusiou.com/api/doc/viewdoc.api';
 			$.ajax({url: url, data:{code:code, lang:lang},
 				dataType:'json', 
 				method:'POST',
