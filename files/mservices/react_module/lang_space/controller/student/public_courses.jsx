3a3fdab7f26d1cd68d329ae9c41a7870d4d98214
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 13:45:49 -0700

    Update public_courses.jsx

--- files/mservices/react_module/lang_space/controller/student/public_courses.jsx
+++ files/mservices/react_module/lang_space/controller/student/public_courses.jsx
@@ -52,7 +52,7 @@ try {
 			}
 		},
 		videoImageFilm:function(a, t) {
-			var url = shusiou_config.api_server + '/api/shusiou_video_image.api?video='+a+'|'+t;
+			var url = shusiou_config.api_server + '/api/video/shusiou_video_image.api?video='+a+'|'+t;
 			return url;
 		},
 		bgFilmStyle:function(t) {
