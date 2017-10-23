69611d7e54b2a376402bb3b7a2fa822fd97fc580
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 12:34:45 -0700

    Update shusiou_video_tmp_section.js

--- api/video/shusiou_video_tmp_section.js
+++ api/video/shusiou_video_tmp_section.js
@@ -4,7 +4,7 @@ if (!req.query['video']) {
 }
 var video = req.query['video'].split('|'), fn;
 
-var folder_base = '/mnt/shusiou-video/youtube/';
+var folder_base = '/mnt/shusiou-video/videos/';
 
 var c_folder = folder_base + video[0] + '/tmp_section/';
 var s_file = folder_base + video[0] + '/video/video.mp4',  s =  video[1], l =  video[2];
