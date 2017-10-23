3f7b9db9b6288800aa236828adae74b970cab415
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 07:28:24 -0700

    Create viewdoc.js

---
+++ api/doc/viewdoc.js
@@ -0,0 +1,40 @@
+// res.send(env); var url = (req.query.md)?req.query.md:req.post.md;
+var source = {
+	Aboutus:	{title:'menu_aboutus', fn:'Aboutus.html'},
+	Contact:	{title:'menu_contact', fn:'Contact.html'},
+	QNA:		{title:'menu_qna', fn:'QNA.html'},
+	Help:		{title:'menu_help', fn:'Help.html'},
+	Testimony:	{title:'menu_testimony', fn:'Testimony.html'},
+	Privacy:	{title:'menu_privacy', fn:'Privacy.html'},
+	Terms:		{title:'menu_terms', fn:'Terms.html'}
+}
+
+if (!source[req.body.code]) {
+	res.send({title: req.body.code, body:' Undefined document <b>' +req.body.code+ '</b>!!'});
+} else {
+	var fn = '/document/' + req.body.lang + '/' + source[req.body.code].fn,
+	fn0 = '/document/en/' + source[req.body.code].fn;
+	pkg.fs.readFile(env.space_path + fn, 'utf8', function(err, contents) {
+		if (err) {
+			pkg.fs.readFile(env.space_path + fn0, 'utf8', function(err, contents) {
+				if (err) {
+					res.send({
+						title: (source[req.body.code])?(source[req.body.code].title):req.body.code,
+						body:'File ' + fn + ' does not exist!'
+					});		   
+				} else {
+					res.send({
+						title: (source[req.body.code])?(source[req.body.code].title):req.body.code,
+						body: contents
+					});
+				}
+			});		   
+		} else {
+			res.send({
+				title: (source[req.body.code])?(source[req.body.code].title):req.body.code,
+				body: contents
+			});
+		}
+	});
+	
+}
