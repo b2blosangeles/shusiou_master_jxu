49b5a7b12e8e5ad8eeb4a1908c751bc80b652899
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 11:12:57 -0700

    Create shusiou_video_tmp_section.js

---
+++ api/video/shusiou_video_tmp_section.js
@@ -0,0 +1,62 @@
+if (!req.query['video']) {
+	res.send('Url error');
+	return true;
+}
+var video = req.query['video'].split('|'), fn;
+
+var folder_base = '/mnt/shusiou-video/youtube/';
+
+var c_folder = folder_base + video[0] + '/tmp_section/';
+var s_file = folder_base + video[0] + '/video/video.mp4',  s =  video[1], l =  video[2];
+var fn = c_folder + s + '_' + l + '.mp4';
+
+var childProcess = require('child_process');
+var CP = new pkg.crowdProcess();
+var _f = {};
+
+_f['S0'] = function(cbk) {
+	var folderP = require(env.site_path + '/api/inc/folderP/folderP');
+	var fp = new folderP();
+	fp.build(c_folder, function() {
+		cbk(c_folder);	
+	});
+	
+};
+_f['S1'] = function(cbk) {
+	pkg.fs.stat(fn, function(err, stat) {
+		var ls = childProcess.exec('ffmpeg  -i ' + s_file + ' -ss '+ s + ' -t ' + l + ' -c copy ' + fn +' -y ', 		   
+			function (error, stdout, stderr) {
+				cbk(true);
+			});
+	});
+}
+
+CP.serial(
+	_f,
+	function(data) {
+		pkg.fs.stat(fn, function(err, data) {
+		    if (err) 
+		      res.send('it does not exist');
+		    else {
+			      var total = data.size;
+			      var range = req.headers.range;
+			      if (range) {
+					var parts = range.replace(/bytes=/, "").split("-");
+					var partialstart = parts[0]; var partialend;
+					  partialend =  parts[1];
+					var start = parseInt(partialstart, 10);
+					var end = partialend ? parseInt(partialend, 10) : total-1;
+					var chunksize = (end-start)+1;
+					var file = pkg.fs.createReadStream(fn, {start:start, end:end});
+
+					res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+						'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+				       file.pipe(res);
+				} else {
+					res.send('Need streaming player');
+				}
+			}
+		});
+	},
+	30000
+);
