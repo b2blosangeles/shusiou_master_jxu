42791130fb92f5bd653eb012bafc1d13f0f23d11
b2blosangeles <b2blosangeles@gmail.com>
2017-10-23 17:37:33 -0700

    Update shusiou_video_image.js

--- api/video/shusiou_video_image.js
+++ api/video/shusiou_video_image.js
@@ -41,7 +41,7 @@ _f['S2'] = function(cbk) {
 		// } else {
 		 
 			var childProcess = require('child_process');
-			var ls = childProcess.exec('ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' + '90' + '  -preset ultrafast ' +  fn +' -y ', 
+			var ls = childProcess.exec('ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' +  w + '  -preset ultrafast ' +  fn +' -y ', 
 			//var ls = childProcess.exec('ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' + w + '  ' +  fn +' -y ',
 				function (error, stdout, stderr) {
 					cbk(true);
