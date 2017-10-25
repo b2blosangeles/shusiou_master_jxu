e1862db828a8ac592ba37ebf9fe4f84171ab9117
b2blosangeles <b2blosangeles@gmail.com>
2017-10-25 10:58:18 -0700

    Update test_image.js

--- api/video/test_image.js
+++ api/video/test_image.js
@@ -42,11 +42,11 @@ _f['S2'] = function(cbk) {
 			//env._itv = setInterval(function() {
 			//	if (!env.ffmpeg) {
 					env.ffmpeg = new Date().getTime();
-		/*
+		
 					var ls = childProcess.spawn('ffmpeg',
 					['-ss', s, '-i', file_video, '-vf', 'scale=-1:' +  w, '-preset', 'ultrafast', fn, '-y']
 					);
-			*/		
+					
 					var dd='';
 					var ls = childProcess.exec('ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' +  w + '  -preset ultrafast ' +  fn +' -y ', 
 						function (error, stdout, stderr) {
