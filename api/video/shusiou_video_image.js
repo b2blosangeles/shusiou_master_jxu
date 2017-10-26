fb84365d9787c114ebd6c46b5504e598829936f8
b2blosangeles <b2blosangeles@gmail.com>
2017-10-25 19:01:05 -0700

    Update shusiou_video_image.js

--- api/video/shusiou_video_image.js
+++ api/video/shusiou_video_image.js
@@ -64,9 +64,9 @@ CP.serial(
 		      if (err) {
 			      res.send(fn + ' does not exist');
 		      } else {
-				var file = pkg.fs.createReadStream(fn);
-				file.pipe(res);	
-			  //    res.sendFile(fn);
+			//	var file = pkg.fs.createReadStream(fn);
+			//	file.pipe(res);	
+			      res.sendFile(fn);
 			}
 		});
 	},
