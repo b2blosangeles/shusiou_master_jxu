8fac31fdd1797ce9201a3a6704499f9fc29106ec
b2blosangeles <b2blosangeles@gmail.com>
2017-10-28 08:10:16 -0700

    Update video_download.js

--- cron_service/video_download.js
+++ cron_service/video_download.js
@@ -135,9 +135,15 @@ _f['D1'] = function(cbk) {
 	var childProcess = require('child_process');
 	var file_video = CP.data.DR1 +'video.mp4', w = 180, s = 10, f_n = CP.data.DR2 + w + '_' + s + '_.png';
 	
-	var s = 'ffmpeg ';
+	
+	var s1 = 'ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' +  180 + '  -preset ultrafast ' +  CP.data.DR2 + 180 + '_' + s + '_.png' +' ';
+	var s2 = ' -vf scale=-1:' +  90 + '  -preset ultrafast ' + CP.data.DR2 + 90 + '_' + s + '_.png'+' ';
+	var s2 = ' -vf scale=-1:' +  480 + '  -preset ultrafast ' + CP.data.DR2 + 480 + '_' + s + '_.png'+' ';
+	var s3 = ' -vf scale=-1:' +  360 + '  -preset ultrafast ' +  CP.data.DR2 + 360 + '_' + s + '_.png'+' -y ;';	
+	
+	var s = 'ffmpeg -i ' + file_video;
 	for (var i = 0; i < 100; i++) {
-		s+= ' -ss ' + i + ' -i ' + file_video + ' -vf scale=-1:' +  180 + '  -preset ultrafast ' +  CP.data.DR2 + 180 + '_' + i + '_.png' +' ';
+		s+= ' -ss ' + i + ' -vf scale=-1:' +  180 + '  -preset ultrafast ' +  CP.data.DR2 + 180 + '_' + i + '_.png' +' ';
 	}
 	s += ' -y'
 	
