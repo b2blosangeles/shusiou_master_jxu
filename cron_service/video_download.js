5e507056b3bee7f02ff511abeaece061328dabbc
b2blosangeles <b2blosangeles@gmail.com>
2017-10-28 08:12:55 -0700

    Update video_download.js

--- cron_service/video_download.js
+++ cron_service/video_download.js
@@ -135,8 +135,8 @@ _f['D1'] = function(cbk) {
 	var childProcess = require('child_process');
 	var file_video = CP.data.DR1 +'video.mp4', w = 180, s = 10, f_n = CP.data.DR2 + w + '_' + s + '_.png';
 	
-	
-	var s1 = 'ffmpeg -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' +  180 + '  -preset ultrafast ' +  CP.data.DR2 + 180 + '_' + s + '_.png' +' ';
+	var s0 = 'ffmpeg ';
+	var s1 = ' -ss ' + s + ' -i ' + file_video + ' -vf scale=-1:' +  180 + '  -preset ultrafast ' +  CP.data.DR2 + 180 + '_' + s + '_.png' +' ';
 	var s2 = ' -vf scale=-1:' +  90 + '  -preset ultrafast ' + CP.data.DR2 + 90 + '_' + s + '_.png'+' ';
 	var s2 = ' -vf scale=-1:' +  480 + '  -preset ultrafast ' + CP.data.DR2 + 480 + '_' + s + '_.png'+' ';
 	var s3 = ' -vf scale=-1:' +  360 + '  -preset ultrafast ' +  CP.data.DR2 + 360 + '_' + s + '_.png'+' -y ;';	
@@ -147,7 +147,7 @@ _f['D1'] = function(cbk) {
 	}
 	s += ' -y'
 	
-	var ls = childProcess.exec(s, 
+	var ls = childProcess.exec(s0 + s1 + s2 + s3, 
 		function (error, stdout, stderr) {
 		  cbk('=niu=');		
 		});	
