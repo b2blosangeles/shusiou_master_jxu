a172740079e94f04d81d76cfc090916dfedec6f1
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 11:03:45 -0700

    Update resume_download.js

--- api/resume_download.js
+++ api/resume_download.js
@@ -16,25 +16,6 @@ var holder_ip = getServerIP();
 var CP = new pkg.crowdProcess();
 var _f = {};
 
-_f['P0'] = function(cbk) {
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-	var str = 'SELECT * FROM `download_success` WHERE 1';
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk('falseB');
-		} else {
-			if (results.length) {
-				cbk(results);
-			} else {
-				cbk('falseA');
-			}
-
-		}
-	});  
-};
-
 _f['P1'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
@@ -72,8 +53,27 @@ _f['P2'] = function(cbk) {
 		}
 	});  
 };
+/*
 
+_f['P2'] = function(cbk) {
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
+	var str = 'INSERT INTO `download_queue` (`source`, `code`, `info`) SELECT `source`, `code`, `video_info` FROM `download_success` ';
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk('falseB');
+		} else {
+			if (results) {
+				cbk(results);
+			} else {
+				cbk('falseA');
+			}
 
+		}
+	});  
+};
+*/
 _f['P3'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
