7c58370a5758a6c18ac668337f0b103207e2168b
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 16:33:07 -0700

    Update download_queue.js

--- api/video/download_queue.js
+++ api/video/download_queue.js
@@ -1,13 +1,14 @@
 var ytdl = require(env.site_path + '/api/inc/ytdl-core/node_modules/ytdl-core'),
     mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql');
 
-var opt = req.query['opt'],
-    uid = req.body.uid | 1, 
-    source = req.body.source | 'ytdl-core',
-    code = req.body.code | 'https://youtu.be/K7AUKcrIdWU';
+var opt = req.query['opt'];
 
 switch(opt) {
 	case 'add':
+		var uid = req.body.uid | 1, 
+		    source = req.body.source | 'ytdl-core',
+		    code = req.body.code | 'https://youtu.be/K7AUKcrIdWU';
+		
 		var CP = new pkg.crowdProcess();
 		var _f = {};
 		_f['P0'] = function(cbk) {
