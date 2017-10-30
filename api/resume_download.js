0d835eb69a3cc052e1a653e35e38e091aea81a53
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 16:04:10 -0700

    Update resume_download.js

--- api/resume_download.js
+++ api/resume_download.js
@@ -1,10 +1,9 @@
 var 	ytdl = require(env.site_path + '/api/inc/ytdl-core/node_modules/ytdl-core'),
 	mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-	cfg0 = require(env.site_path + '/api/cfg/db.json');
+	cfg0 = require(env.site_path + '/api/cfg/db.json'),
+    	CP = new pkg.crowdProcess();
 
-var CP = new pkg.crowdProcess();
 var _f = {};
-
 _f['P1'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
@@ -27,26 +26,6 @@ _f['P1'] = function(cbk) {
 _f['P2'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
-	var str = 'INSERT INTO `download_queue` (`source`, `code`, `info`) SELECT `source`, `code`, `video_info` FROM `download_failure` ';
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk('falseB');
-		} else {
-			if (results) {
-				cbk(results);
-			} else {
-				cbk('falseA');
-			}
-
-		}
-	});  
-};
-/*
-
-_f['P2'] = function(cbk) {
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
 	var str = 'INSERT INTO `download_queue` (`source`, `code`, `info`) SELECT `source`, `code`, `video_info` FROM `download_success` ';
 	connection.query(str, function (error, results, fields) {
 		connection.end();
@@ -62,25 +41,7 @@ _f['P2'] = function(cbk) {
 		}
 	});  
 };
-*/
-_f['P3'] = function(cbk) {
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-	var str = 'UPDATE `download_queue` SET `status` = 0, `holder_ip` = "", `hold_time` = NULL';
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk('falseB');
-		} else {
-			if (results.length) {
-				cbk(results[0]);
-			} else {
-				cbk('falseA');
-			}
 
-		}
-	});  
-};
 CP.serial(
 	_f,
 	function(data) {
