8a88fdd2c0924319cc5d1604785e41dbb3c2fcd7
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 16:32:18 -0700

    Update download_queue.js

--- api/video/download_queue.js
+++ api/video/download_queue.js
@@ -6,49 +6,54 @@ var opt = req.query['opt'],
     source = req.body.source | 'ytdl-core',
     code = req.body.code | 'https://youtu.be/K7AUKcrIdWU';
 
-var CP = new pkg.crowdProcess();
-var _f = {};
-_f['P0'] = function(cbk) {
-	ytdl.getInfo(code, {},  function(err, info){	
-	  if (err) {  
-		cbk(false);  	  
-	  } else {
-		cbk({vid:info.video_id, title:info.title + "asd'asd", length_seconds:parseInt(info.length_seconds), thumbnail_url:info.thumbnail_url});
-	  }
-	});	  
-};
+switch(opt) {
+	case 'add':
+		var CP = new pkg.crowdProcess();
+		var _f = {};
+		_f['P0'] = function(cbk) {
+			ytdl.getInfo(code, {},  function(err, info){	
+			  if (err) {  
+				cbk(false);  	  
+			  } else {
+				cbk({vid:info.video_id, title:info.title + "asd'asd", length_seconds:parseInt(info.length_seconds), thumbnail_url:info.thumbnail_url});
+			  }
+			});	  
+		};
 
-_f['P1'] = function(cbk) {
-	var cfg0 = require(env.site_path + '/api/cfg/db.json');
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-	
-	var str = 'INSERT INTO `download_queue` (`source`, `code`, `uid`, `info`,`created`, `status`) ' +
-				"values ('" + source + "', '" + code.replace(/\'/g, "\\\'") + "', '" + uid + "', "+
-	    			"'" + JSON.stringify(CP.data.P0).replace(/\'/g, "\\\'")  + "', " + 
-				'NOW(), 0 ); ';
-	
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk(str);
-		} else {
-			if (results.length) {
-				cbk(results[0]);
-			} else {
-				cbk(true);
-			}
+		_f['P1'] = function(cbk) {
+			var cfg0 = require(env.site_path + '/api/cfg/db.json');
+			var connection = mysql.createConnection(cfg0);
+			connection.connect();
 
-		}
-	});  
-};
+			var str = 'INSERT INTO `download_queue` (`source`, `code`, `uid`, `info`,`created`, `status`) ' +
+						"values ('" + source + "', '" + code.replace(/\'/g, "\\\'") + "', '" + uid + "', "+
+						"'" + JSON.stringify(CP.data.P0).replace(/\'/g, "\\\'")  + "', " + 
+						'NOW(), 0 ); ';
 
-CP.serial(
-	_f,
-	function(data) {
-		res.send(CP.data.P1);
-		// res.send({_spent_time:data._spent_time, status:data.status, data:data});
-	},
-	30000
-);
+			connection.query(str, function (error, results, fields) {
+				connection.end();
+				if (error) {
+					cbk(str);
+				} else {
+					if (results.length) {
+						cbk(results[0]);
+					} else {
+						cbk(true);
+					}
 
+				}
+			});  
+		};
+
+		CP.serial(
+			_f,
+			function(data) {
+				res.send(CP.data.P1);
+				// res.send({_spent_time:data._spent_time, status:data.status, data:data});
+			},
+			30000
+		);
+		break;
+	default:
+        	res.send('Wrong opt value!');
+}
