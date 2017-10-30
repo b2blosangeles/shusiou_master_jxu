3afcfef84929191c6f9a057b661b8eeed835164f
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 16:34:00 -0700

    Update download_queue.js

--- api/video/download_queue.js
+++ api/video/download_queue.js
@@ -55,6 +55,45 @@ switch(opt) {
 			30000
 		);
 		break;
+	case 'getQueue':
+		var uid = req.body.uid | 1;
+		
+		var CP = new pkg.crowdProcess();
+		var _f = {};
+		_f['P1'] = function(cbk) {
+			var cfg0 = require(env.site_path + '/api/cfg/db.json');
+			var connection = mysql.createConnection(cfg0);
+			connection.connect();
+
+			var str = 'INSERT INTO `download_queue` (`source`, `code`, `uid`, `info`,`created`, `status`) ' +
+						"values ('" + source + "', '" + code.replace(/\'/g, "\\\'") + "', '" + uid + "', "+
+						"'" + JSON.stringify(CP.data.P0).replace(/\'/g, "\\\'")  + "', " + 
+						'NOW(), 0 ); ';
+
+			connection.query(str, function (error, results, fields) {
+				connection.end();
+				if (error) {
+					cbk(str);
+				} else {
+					if (results.length) {
+						cbk(results[0]);
+					} else {
+						cbk(true);
+					}
+
+				}
+			});  
+		};
+
+		CP.serial(
+			_f,
+			function(data) {
+				res.send(CP.data.P1);
+				// res.send({_spent_time:data._spent_time, status:data.status, data:data});
+			},
+			30000
+		);
+		break;		
 	default:
         	res.send('Wrong opt value!');
 }
