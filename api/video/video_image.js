f99d4b40c565d343912ed6005e3422934e149ec0
b2blosangeles <b2blosangeles@gmail.com>
2017-10-31 08:36:30 -0700

    Update video_image.js

--- api/video/video_image.js
+++ api/video/video_image.js
@@ -16,6 +16,18 @@ var s=req.query['s'], fn = folder_image + w + '_' + s + '.png';
 var CP = new pkg.crowdProcess();
 
 var _f = {};
+
+_f['I0'] = function(cbk) { /* --- check mnt exist --- */
+	pkg.fs.stat(mnt_folder, function (err, stats){
+		if (err) { cbk(false); CP.exit = 1;}
+		else if (!stats.isDirectory()) {
+			cbk(false); CP.exit = 1;
+		} else {
+			cbk(true);
+		}
+	});
+};
+
 _f['S0'] = function(cbk) {
 	pkg.fs.stat(file_video, function(err, stat) {
 		 if(!err) {
