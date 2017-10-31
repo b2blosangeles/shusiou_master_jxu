c446215f8370a6a3b3010105a963be399ccf9158
b2blosangeles <b2blosangeles@gmail.com>
2017-10-31 11:07:24 -0700

    Update myVideo.js

--- api/video/myVideo.js
+++ api/video/myVideo.js
@@ -18,7 +18,7 @@ switch(opt) {
 			connection.connect();
 
 			// var str = "SELECT A.`id`, A.`code`, B.`uid`, B.`video_code`, B.`created`"+
-			var str = "SELECT B.`id`"+    
+			var str = "SELECT * FROM `video_user` WHERE `id` IS IN SELECT B.`id`"+    
 				" FROM `download_failure` A LEFT JOIN `video_user` B ON A.`id` = B.`video_code` " +
 				" WHERE B.`uid` = '" + uid + "' AND NOW() - B.`created` < 86400; ";
 			connection.query(str, function (error, results, fields) {
