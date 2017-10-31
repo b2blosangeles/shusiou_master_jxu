000d5fd48c514d14f5ee4895cb6f75cae89f71cf
b2blosangeles <b2blosangeles@gmail.com>
2017-10-30 22:03:18 -0700

    Update myVideo.js

--- api/video/myVideo.js
+++ api/video/myVideo.js
@@ -8,7 +8,7 @@ switch(opt) {
 	case 'add':
 		var uid = req.body.uid || 1, 
 		    source = req.body.source || 'ytdl-core',
-		    code = req.body.code || 'https://youtu.be/9thW5hTOMTM';
+		    code = req.body.code || 'https://youtu.be/CmVty_4qFZw';
 
 		var CP = new pkg.crowdProcess();
 		var _f = {};
