421f3f7cb28803991226d752062177f913d9ff85
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 06:14:53 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -94,12 +94,12 @@ try {
 			}});
 			return true;
 		},			
-		videoDelete:function(vid){
+		videoDelete:function(video_code){
 			var me = this;
 			$.ajax({
 				url: shusiou_config.api_server + '/api/video/myVideo.api?opt=removeUserVideo',
 				method: "POST",
-				data: {auth: me.props.route.env.state.auth},
+				data: {video_code:video_code, auth: me.props.route.env.state.auth},
 				dataType: "JSON"
 			}).done(function( data) {
 				me.pullList();
