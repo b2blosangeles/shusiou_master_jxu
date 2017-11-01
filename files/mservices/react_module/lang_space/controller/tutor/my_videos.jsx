d66c9e8ed37649ddf353c51028fa1193f299cefd
b2blosangeles <b2blosangeles@gmail.com>
2017-10-31 17:35:39 -0700

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -144,7 +144,7 @@ try {
 							</div>			
 						</div>
 						{me.state.list.map(function(a){ 
-							if (a.type == 'local') return(
+							if (a.type != 'local') return(
 							<div className="col-sm-4 col-lg-4 col-md-4"> 
 								<div className="overlayer_box homepage_box" style={{'margin-bottom':'1em', 'padding':'0.5em'}}>
 									<div className="video_thumbnail_icon_group">
