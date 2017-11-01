cc0c1e7253181e3aaec46542e82d522d1fee058a
b2blosangeles <b2blosangeles@gmail.com>
2017-10-31 20:12:48 -0700

    Update ad.jsx

--- files/mservices/react_module/lang_space/view/ad.jsx
+++ files/mservices/react_module/lang_space/view/ad.jsx
@@ -42,7 +42,8 @@ try {
 			var me = this;
 			var idx = Math.floor(Math.random()*me.state.adlist.length);
 		//	var l = shusiou_config.api_server + '/api/video/shusiou_video_section.api?video='+me.state.adlist[idx].code+'|15|30';
-			var l = 'http://' + me.state.adlist[idx].host + '/api/video/shusiou_video_section.api?video='+me.state.adlist[idx].code+'|15|30';
+			var l = 'http://' + me.state.adlist[idx].server_ip + '/api/video/play_video_section.api?vid='+
+			    me.state.adlist[idx].video_code+'&s=30&l=30';
 			$('.content_bg').find('video').attr("src", l);
 		},
 		render: function() {
