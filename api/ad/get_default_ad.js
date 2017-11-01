b56a6ed28fee41c6ce0fa1f9054284097066b702
b2blosangeles <b2blosangeles@gmail.com>
2017-10-31 20:07:28 -0700

    Update get_default_ad.js

--- api/ad/get_default_ad.js
+++ api/ad/get_default_ad.js
@@ -8,8 +8,7 @@ _f['Q0'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 
-	var str = 'SELECT A.*, B.nodes FROM  `videos` A, `video_node` B WHERE A.id = B.video_id '+
-	    ' AND B.nodes <> "" LIMIT 3';
+	var str = 'SELECT * FROM  `video` WHERE 1 LIMIT 3';
 
 	connection.query(str, function (error, results, fields) {
 		connection.end();
