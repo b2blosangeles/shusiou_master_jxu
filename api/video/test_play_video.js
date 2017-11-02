be686a1369da4a11d40fe395ef5fd6ac55e67e3b
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 18:00:33 -0700

    Update test_play_video.js

--- api/video/test_play_video.js
+++ api/video/test_play_video.js
@@ -1,9 +1,9 @@
 var request = require(env.root_path + '/package/request/node_modules/request');
 var file = pkg.fs.createWriteStream('/tmp/video_a.mp4');
 
-var request = http.get('http://shusiou.com/api/video/pipe_stream.js?fn='+cg[i], function(response) {
+var request = http.get('http://shusiou.com/api/video/test_pipe.api?', function(response) {
 	response.pipe(file);
 	response.on('end', function() {
-		res.send('pulled ' + cg[i] + ' at: ' + (new Date().getTime() - tm) + ' ms');	
+		res.send('pulled  at: ' + (new Date().getTime() - tm) + ' ms');	
 	});
 });
