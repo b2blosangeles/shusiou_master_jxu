f3ddfef273bf049b817a72812decf1bec2c7c75a
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 18:06:02 -0700

    Update test_play_video.js

--- api/video/test_play_video.js
+++ api/video/test_play_video.js
@@ -1,5 +1,6 @@
 var request = require(env.root_path + '/package/request/node_modules/request');
 var file = pkg.fs.createWriteStream('/tmp/video_a.mp4');
+var http = require('http');
 
 var request = http.get('http://shusiou.com/api/video/test_pipe.api?', function(response) {
 	response.pipe(file);
