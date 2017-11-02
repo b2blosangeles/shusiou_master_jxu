5b175bb2fdfeca8c23e89a13a1e0a5a9dc0b500e
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 14:24:58 -0700

    Update hub_info.js

--- api/video/hub_info.js
+++ api/video/hub_info.js
@@ -6,9 +6,9 @@ var mnt_folder = '/mnt/shusiou-video/';
 var fn = mnt_folder + 'videos/'+req.query['vid']+'/video/video.mp4';
 
 pkg.fs.stat(fn, function(err, data) {
-  if (err) {
-   res.send('NO');
-  } else {
-    res.send(data.size);
-  }
+	if (err) {
+		res.send({status:'error', message:'File does not exist!'});
+	} else {
+		res.send({status:'success', size:data.size});
+	}
 });
