ea7d3a0a14e69989ea291bea4fd469e137863a5c
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 18:08:13 -0700

    Update test_play_video.js

--- api/video/test_play_video.js
+++ api/video/test_play_video.js
@@ -5,6 +5,6 @@ var http = require('http');
 var request = http.get('http://shusiou.com/api/video/test_pipe.api?', function(response) {
 	response.pipe(file);
 	response.on('end', function() {
-		res.send('pulled  at: ' + (new Date().getTime() - tm) + ' ms');	
+		res.send('pulled  at: ' + new Date().getTime() + ' ms');	
 	});
 });
