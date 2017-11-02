fd6ae83629bde1256c6cbc1fbe4d21e7fbc132d8
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 17:59:07 -0700

    Create test_pipe.js

---
+++ api/video/test_pipe.js
@@ -0,0 +1,34 @@
+var fn = '/var/log/cron_git.log';
+pkg.fs.stat(fn, function(err, data) {
+	if (err) {
+		//      res.redirect('http://api.shusiou.com'+req.url);
+		res.send('NO');
+	} else {
+
+		var stream = require("stream")
+		var s = new stream.PassThrough();
+		s.push('*** Current view time:' + new Date().toString() + " *** \n\n");
+		s.end()		
+		/*
+		var Readable = require('stream').Readable;
+		var s = new Readable();
+		s.push('*** 5 Current view time:' + new Date().toString() + " *** \n\n");
+		s.push(null);
+		*/
+		
+		var readerStream1 = pkg.fs.createReadStream(fn);
+		var readerStream2 = pkg.fs.createReadStream(fn);
+		s.on('end', function(){
+       			 readerStream1.pipe(res, { end:false});
+    		});		
+		readerStream1.on('end', function(){
+       			 readerStream2.pipe(res, { end:false});
+    		});
+		
+		readerStream2.on('end', function(){
+			res.end();
+		    });
+		    
+		s.pipe(res, { end:false});
+	}
+});
