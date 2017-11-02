e343d6abf9e04db97104abfcfa095e1e8e910b57
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 14:21:11 -0700

    Create hub_info.js

---
+++ api/video/hub_info.js
@@ -0,0 +1,14 @@
+if (!req.query['vid']) {
+	res.send('vid error ');
+	return true;
+}
+var mnt_folder = '/mnt/shusiou-video/';
+var fn = mnt_folder + 'videos/'+req.query['vid']+'/video/video.mp4';
+
+pkg.fs.stat(fn, function(err, data) {
+  if (err) {
+   res.send('NO');
+  } else {
+    res.send(data.size);
+  }
+});
