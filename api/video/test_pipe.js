20d86960050b54664efb78494962931d5eaeb287
b2blosangeles <b2blosangeles@gmail.com>
2017-11-01 21:22:57 -0700

    Update test_pipe.js

--- api/video/test_pipe.js
+++ api/video/test_pipe.js
@@ -1,9 +1,13 @@
+
+if (!req.query['vid']) {
+	res.send('vid error ');
+	return true;
+}
 var mnt_folder = '/mnt/shusiou-video/';
-var fn = mnt_folder + 'videos/1/video/video.mp4';
+var fn = mnt_folder + 'videos/'+req.query['vid']+'/video/video.mp4';
 
 pkg.fs.stat(fn, function(err, data) {
 	if (err) {
-		//      res.redirect('http://api.shusiou.com'+req.url);
 		res.send('NO');
 	} else {
 		var readerStream = pkg.fs.createReadStream(fn);
