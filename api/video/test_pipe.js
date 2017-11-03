5340dfcc1ee737dc51b5a9964e585546631c7656
b2blosangeles <b2blosangeles@gmail.com>
2017-11-02 21:17:29 -0700

    Update test_pipe.js

--- api/video/test_pipe.js
+++ api/video/test_pipe.js
@@ -1,10 +1,10 @@
 
-if (!req.query['vid']) {
-	res.send('vid error ');
+if (!req.query['fn']) {
+	res.send('Error, Missing fn ');
 	return true;
 }
 var mnt_folder = '/mnt/shusiou-video/';
-var fn = mnt_folder + 'videos/'+req.query['vid']+'/video/video.mp4';
+var fn = mnt_folder + req.query['fn'];
 
 pkg.fs.stat(fn, function(err, data) {
 	if (err) {
