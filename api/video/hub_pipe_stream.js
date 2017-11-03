28bc43f879cdcf71d2e1ed6f8a76297efcb3c279
b2blosangeles <b2blosangeles@gmail.com>
2017-11-03 11:31:34 -0700

    Update hub_pipe_stream.js

--- api/video/hub_pipe_stream.js
+++ api/video/hub_pipe_stream.js
@@ -9,7 +9,7 @@ var fn = mnt_folder + req.query['fn'];
 pkg.fs.stat(fn, function(err, data) {
 	if ((err) || data.size < 1024) {
 		res.writeHead(404);
-		res.write(fn.replace(new RegExp('^'+mnt_folder,'i'),'==') + ' does not exist or size is too small');
+		res.write(fn.replace(new RegExp('^'+mnt_folder,'i'),'./') + ' does not exist or size is too small');
 		res.end();
 	} else {
 		res.writeHead(200); 
