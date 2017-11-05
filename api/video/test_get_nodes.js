4983fdcd4d8564b528d27ccd09aa90b80c95e059
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 09:17:52 -0800

    Update test_get_nodes.js

--- api/video/test_get_nodes.js
+++ api/video/test_get_nodes.js
@@ -7,7 +7,7 @@ switch(opt) {
 	case 'getVideoNode':
 		delete require.cache[env.site_path + '/api/inc/videoNode/videoNode.js'];
 		var videoNode = require(env.site_path + '/api/inc/videoNode/videoNode.js');
-		var vn = new  videoNode();
+		var vn = new  videoNode(env);
 		vn.getIP(1, function(data){
 			res.send(data);
 		});
