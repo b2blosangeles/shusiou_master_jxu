d85351fa8ef9298c4b0af8f76d828c5773f01e64
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 08:58:06 -0800

    Update test_get_nodes.js

--- api/video/test_get_nodes.js
+++ api/video/test_get_nodes.js
@@ -5,6 +5,11 @@ var opt = req.query['opt'];
 
 switch(opt) {
 	case 'getVideoNode':
+		var videoNode = require(env.site_path + '/api/inc/videoNode/videoNode.js');
+		var vn = new  videoNode();
+		vn.getIP(1, function(data){
+			res.send(data);
+		});
 		res.send(opt);
 		return true;
 		var vid = req.query['vid'];
