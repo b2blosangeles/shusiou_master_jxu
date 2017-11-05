f54e7d8c3e61dbdbc23581e981b0425baa2a9bab
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 09:51:36 -0800

    Update videoNode.js

--- api/inc/videoNode/videoNode.js
+++ api/inc/videoNode/videoNode.js
@@ -64,10 +64,10 @@
 					connection.connect();	
 					connection.query(str, function (error, results, fields) {
 						connection.end();
-						if (error) { cbk(str); } 
+						if (error) { cbk(false); } 
 						else if (results) { 
-							cbk(results);
-						} else { cbk(str + '==');  }
+							cbk(true);
+						} else { cbk(false);  }
 					});  
 				} else {
 					cbk(false);
@@ -78,7 +78,7 @@
 				function(data) {
 					var d = [];
 					if (CP.data.P0) d = d.concat(CP.data.P0);
-					if (CP.data.P1) d = d.concat(CP.data.P1);
+					if ((CP.data.P1) && (CP.data.P2)) d = d.concat(CP.data.P1);
 					callback({status:'success', _spent_time:data._spent_time, data:d});
 				},
 				6000
