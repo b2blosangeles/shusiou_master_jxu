b7d2b799a1e216522efaa6270035a9df64616770
b2blosangeles <b2blosangeles@gmail.com>
2017-11-05 16:42:50 -0800

    Update test_get_nodes.js

--- api/video/test_get_nodes.js
+++ api/video/test_get_nodes.js
@@ -16,6 +16,18 @@ switch(opt) {
 		});
 		return true;
 		break;
+	case 'getVideoNodeStatus':
+		delete require.cache[env.site_path + '/api/inc/videoNode/videoNode.js'];
+		var videoNode = require(env.site_path + '/api/inc/videoNode/videoNode.js');
+		
+		var vid = req.query['vid'];
+		
+		var vn = new  videoNode(env, pkg);
+		vn.getIP(vid, function(data){
+			res.send(data);
+		});
+		return true;
+		break;		
 	default:
 		res.send({status:'error', message:'Wrong opt value!'});
 }
