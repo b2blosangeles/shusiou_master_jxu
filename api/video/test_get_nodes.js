4ef10d25fcf2061540d5c116d81ae4eb3ba35cd1
b2blosangeles <b2blosangeles@gmail.com>
2017-11-08 08:37:38 -0800

    Update test_get_nodes.js

--- api/video/test_get_nodes.js
+++ api/video/test_get_nodes.js
@@ -104,43 +104,11 @@ switch(opt) {
 		CP_s.serial(
 			_f_s,
 			function(data_s) {
-				res.send(data_s.results);
+				res.send(local_flist:CP.data.local_flist, data_s.results);
 			},
 			6000
-		);		
-		return true;		
-		
-		var connection = mysql.createConnection(cfg0);
-		connection.connect();
-		var str = "SELECT A.`node_ip`, A.`vid`, A.`status` FROM `video_node` A  LEFT JOIN `video` B ON A.`vid` = B.`video_code`  WHERE 1 ";
-		connection.query(str, function (error, results, fields) {
-			connection.end();
-			if (error) { res.send(false); } 
-			else if (results) { 
-				res.send(results);
-			} else { res.send(false); }
-		});  
-		return true;
-		
-		delete require.cache[env.site_path + '/api/inc/videoNode/videoNode.js'];
-		var videoNode = require(env.site_path + '/api/inc/videoNode/videoNode.js');
-
-		var vid = req.query['vid'];
-		
-		var mnt_folder = '/mnt/shusiou-video/';
-		var fn = mnt_folder + 'videos/' + vid + '/video/video.mp4';
-		
-
-		
-		
-		var vn = new  videoNode(env, pkg);
-		vn.getIP(vid, function(data){
-			pkg.fs.stat(fn, function(err, stat) {
-				res.send({ips:data.list, state:stat.size});
-			});			
-		});
-		return true;
-		break;		
+		);
+			
 	default:
 		res.send({status:'error', message:'Wrong opt value!'});
 }
