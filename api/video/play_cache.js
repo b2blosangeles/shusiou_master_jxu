338f7561f62662fe533638b0fb73c5165df257bc
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 14:51:03 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -8,7 +8,7 @@ var ytdl = require(env.site_path + '/api/inc/ytdl-core/node_modules/ytdl-core'),
 var connection = mysql.createConnection(cfg0);
 	connection.connect();
 	var str = "SELECT A.* "+    
-	    	 " FROM `video_node` A LEFT JOIN `video` B ON A.`vid` = B.`video_code ` " +
+	    	 " FROM `video_node` A LEFT JOIN `video` B ON A.`vid` = B.`video_code` " +
 	     	// " FROM `video` A  " + 
 		" WHERE A.`vid` = '" + vid + "'; ";
 	connection.query(str, function (error, results, fields) {
