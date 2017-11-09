9111767f360d55210698307c7076bd8e481deede
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 12:41:48 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -66,10 +66,10 @@ switch(type) {
 		CP.serial(
 			_f,
 			function(data) {
-			//	if (CP.data.S0 !== true) {
+				if (CP.data.S0 !== true) {
 					res.send(CP.data.S0);
 					return true;
-			//	}
+				}
 				pkg.fs.stat(fn, function(err, data1) {
 					if (err) {  write404(fn + ' does not exist'); }
 					else {
