fc91e49ff587421669f84621a1cfca7f468b42be
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 15:18:03 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -11,10 +11,10 @@ var connection = mysql.createConnection(cfg0);
 	    	 " FROM `video_node` A LEFT JOIN `video` B ON A.`vid` = B.`video_code` " +
 	         " WHERE A.`vid` = '" + vid + "'; ";
 		// " WHERE A.`vid` = '" + vid + "' AND A.`status` = '1'; ";
-	 str = "SELECT * "+    
-	    	 " FROM `video_node`  " + '';
+	 str = "SELECT A.* "+    
+	    	 " FROM `video_node` A  " + '';
 	      //   " WHERE A.`vid` = '" + vid + "'; ";
-		// " WHERE A.`vid` = '" + vid + "' AND A.`status` = '1'; ";
+		 " WHERE A.`vid` = '" + vid + "'; ";
 	connection.query(str, function (error, results, fields) {
 		connection.end();
 		if (error)  res.send(false);
