60b24c625893ba206104f01d4cd8cbbc9896cfbd
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 09:45:19 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -11,7 +11,7 @@ var mnt_folder = '/mnt/shusiou-video/',
     video_folder = mnt_folder  + 'videos/' + vid + '/', 
     file_video =  video_folder + 'video/video.mp4',
     folder_image = video_folder + 'images/',
-    folder_section =   video_folder + + 'sections/';
+    folder_section =   video_folder + 'sections/';
 
 var folderP = require(env.site_path + '/api/inc/folderP/folderP');
 
@@ -70,28 +70,25 @@ switch(type) {
 		break;
 	case 'section':
 		var l = req.query['l'], s = req.query['s'];
-		if (!s || ['90', '180', '480', 'FULL'].indexOf(w) === -1) { write404('wrong s or w'); return true; }
-		var fn = folder_image + w + '_' + s + '.png';
+		if (!s || !l) { write404('wrong s or l'); return true; }
+		var fn = folder_section + s + '_' + l + '.mp4';
 
 		var CP = new pkg.crowdProcess();
 		var _f = {};
 
 		_f['S1'] = function(cbk) { 
 			var fp = new folderP();
-			fp.build(folder_image, function() { cbk(true);});
+			fp.build(folder_section, function() { cbk(true);});
 		};
 
 		_f['S2'] = function(cbk) {
 			pkg.fs.stat(fn, function(err, stat) {
 				if(!err) { cbk(fn);
 				} else {
-					if (w != 'FULL') s = 'ffmpeg -ss ' + s + ' -i ' + file_video +' -vf scale=-1:' +  w + '  -preset ultrafast ' + fn + ' -y ';
-					else s = 'ffmpeg -ss ' + s + ' -i ' + file_video +' -vframes 1 ' +  fn + ' -y ';
-					var childProcess = require('child_process');
-					var ls = childProcess.exec(s, 		   
-					function (error, stdout, stderr) {
-						cbk(true);
-					});
+					var ls = childProcess.exec('ffmpeg  -i ' + file_video + ' -ss '+ s + ' -t ' + l + ' -c copy ' + fn +' -y ', 		   
+						function (error, stdout, stderr) {
+							cbk(true);
+						});
 				}
 			});
 		};
@@ -102,9 +99,22 @@ switch(type) {
 				pkg.fs.stat(fn, function(err, data1) {
 					if (err) {  write404(fn + ' does not exist'); }
 					else {
-						res.writeHead(200, {'Content-Type': 'image/png'}); 
-						var file = pkg.fs.createReadStream(fn);
-						file.pipe(res);
+					      var total = data.size;
+					      var range = req.headers.range;
+					      if (range) {
+							var parts = range.replace(/bytes=/, "").split("-");
+							var partialstart = parts[0]; var partialend;
+							  partialend =  parts[1];
+							var start = parseInt(partialstart, 10);
+							var end = partialend ? parseInt(partialend, 10) : total-1;
+							var chunksize = (end-start)+1;
+							var file = pkg.fs.createReadStream(fn, {start:start, end:end});
+							res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+								'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+						       file.pipe(res);
+						} else {
+							res.send('Need streaming player');
+						}
 					}
 				});
 			},
