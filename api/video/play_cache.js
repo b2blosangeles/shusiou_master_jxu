14c3e0263564d00e72c4791eeb84b9c8b8e40ab8
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 14:31:33 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -1,3 +1,24 @@
+var ytdl = require(env.site_path + '/api/inc/ytdl-core/node_modules/ytdl-core'),
+    mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+    cfg0 = require(env.site_path + '/api/cfg/db.json');
+
+var connection = mysql.createConnection(cfg0);
+	connection.connect();
+	var str = "SELECT * "+    
+		" FROM `video_node` " +
+		" WHERE 1; ";
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error)  res.send(false);
+		else if (results) { 
+			var v = [];
+			for (var i = 0; i < results.length; i++) {
+				v[v.length] = results[i].id;
+			}
+			ews.send(v.join(','));
+		} else res.send(false);
+	});  
+return true;
 res.redirect(req.url.replace('/play_cache.api?', '/play_stream.api?'));
 return true;
 
