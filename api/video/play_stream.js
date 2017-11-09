c85331aa8ac8e77a9d861708b5c72490780fdecb
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 09:33:05 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -68,6 +68,49 @@ switch(type) {
 			6000
 		);    
 		break;
+	case 'section':
+		var w = req.query['w'], s = req.query['s'];
+		if (!s || ['90', '180', '480', 'FULL'].indexOf(w) === -1) { write404('wrong s or w'); return true; }
+		var fn = folder_image + w + '_' + s + '.png';
+
+		var CP = new pkg.crowdProcess();
+		var _f = {};
+
+		_f['S1'] = function(cbk) { 
+			var fp = new folderP();
+			fp.build(folder_image, function() { cbk(true);});
+		};
+
+		_f['S2'] = function(cbk) {
+			pkg.fs.stat(fn, function(err, stat) {
+				if(!err) { cbk(fn);
+				} else {
+					if (w != 'FULL') s = 'ffmpeg -ss ' + s + ' -i ' + file_video +' -vf scale=-1:' +  w + '  -preset ultrafast ' + fn + ' -y ';
+					else s = 'ffmpeg -ss ' + s + ' -i ' + file_video +' -vframes 1 ' +  fn + ' -y ';
+					var childProcess = require('child_process');
+					var ls = childProcess.exec(s, 		   
+					function (error, stdout, stderr) {
+						cbk(true);
+					});
+				}
+			});
+		};
+
+		CP.serial(
+			_f,
+			function(data) {
+				pkg.fs.stat(fn, function(err, data1) {
+					if (err) {  write404(fn + ' does not exist'); }
+					else {
+						res.writeHead(200, {'Content-Type': 'image/png'}); 
+						var file = pkg.fs.createReadStream(fn);
+						file.pipe(res);
+					}
+				});
+			},
+			6000
+		);    
+		break;		
 	default:
 		res.send('type error');  
 }
