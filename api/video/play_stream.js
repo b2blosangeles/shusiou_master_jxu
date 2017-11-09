12e931df74489e4e3e288c1292bf1734dce2ab3a
b2blosangeles <b2blosangeles@gmail.com>
2017-11-09 13:56:30 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -159,7 +159,36 @@ switch(type) {
 			},
 			30000
 		);    
-		break;		
+		break;	
+	case 'video':				
+		pkg.fs.stat(file_video, function(err, data1) {
+			if (err) {  write404(fn + ' does not exist'); }
+			else {
+			      var total = data1.size;
+			      var range = req.headers.range;
+			      if (range) {
+					var parts = range.replace(/bytes=/, "").split("-");
+					var partialstart = parts[0]; var partialend;
+					  partialend =  parts[1];
+					var start = parseInt(partialstart, 10);
+					var end = partialend ? parseInt(partialend, 10) : total-1;
+					var chunksize = (end-start)+1;
+					var file = pkg.fs.createReadStream(file_video, {start:start, end:end});
+					res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
+						'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
+				       file.pipe(res);
+					setTimeout(
+						function() {
+							file.destroy();
+							write404('timeout')
+						}, 30000
+					);
+				} else {
+					res.send('Need streaming player');
+				}
+			}
+		});
+		break;			
 	default:
 		 write404('type error');  
 }
