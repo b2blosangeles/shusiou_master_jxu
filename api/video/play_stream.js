25d628803f0631e4c2fa92db587ad7975609d6c1
b2blosangeles <b2blosangeles@gmail.com>
2017-11-10 00:23:52 -0800

    Update play_stream.js

--- api/video/play_stream.js
+++ api/video/play_stream.js
@@ -214,8 +214,8 @@ switch(type) {
 						connection.connect();
 					//	var str = "UPDATE `master_node_log` SET `finished` = NOW() "+
 					//	    "WHERE `id` = '" + inserted_id + "' ";
-						var str = "INSERT INTO `master_node_log` (`type`, `url`, `finished`) VALUES "+    
-						 " ('" + 'video' + "', '" + 'inserted_id' + "', NOW()) ";
+						var str = "INSERT INTO `master_node_log` (`type`, `url`, `started`) VALUES "+    
+						 " ('" + 'video' + "', '" + inserted_id + "', NOW()) ";
 						connection.query(str, function (error, results, fields) {
 							connection.end();
 						}); 						
