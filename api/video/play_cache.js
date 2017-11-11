e22f2dc4f6f39ffa12489fe34d019145fc5cd333
b2blosangeles <b2blosangeles@gmail.com>
2017-11-10 23:07:37 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -8,7 +8,7 @@ var url = req.url.replace('/play_cache.api', '/play_stream.api');
 var connection = mysql.createConnection(cfg0);
 	connection.connect();
 	var str = "SELECT A.`vid`, A.`node_ip`, B.`server_ip` "+    
-	    	 " FROM `video_node` AS A LEFT JOIN `video` AS B  ON A.`vid` = B.`video_code` " +
+	    	 " FROM `video_node` AS A LEFT JOIN `video` AS B  ON A.`vid` = B.`vid` " +
 		 " WHERE A.`vid` = '" + vid + "' AND A.`status` = '1'; ";
 
 	connection.query(str, function (error, results, fields) {
