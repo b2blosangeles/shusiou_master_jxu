9999b58669771c0ead5db540aad25ef459a771f0
b2blosangeles <b2blosangeles@gmail.com>
2017-11-12 18:38:46 -0800

    Update cron_videoNodeStatus1.js

--- cron_service/cron_videoNodeStatus1.js
+++ cron_service/cron_videoNodeStatus1.js
@@ -37,7 +37,6 @@ _f_s['local_video']  = function(cbk_s) {
 		connection.end();
 		var v = [];
 		for (var i=0; i < results.length; i++) v[v.length] = results[i]['vid'].toString();
-		console.log(v);
 		cbk_s(v);
 	});
 };		
