e5eda138aaf119d4659069d8bc56ef14a6f3a27a
b2blosangeles <b2blosangeles@gmail.com>
2017-11-12 22:08:41 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -70,7 +70,7 @@ CP.serial(
 		var ip = ips[Math.floor(Math.random() * ips.length)], patt = /([?&]server)=([^#&]*)/i;
 		if (patt.test(url)) url = ('http://'+ ip + url).replace(patt,'$1=' + server_ip);
 		else url = 'http://'+ ip + url + '&server=' + server_ip;
-		cbk(url);
+		res.send(url);
 	},
 	30000
 );   
