9d0de2027bd6a274ef00df36010580983426867a
b2blosangeles <b2blosangeles@gmail.com>
2017-11-13 22:27:15 -0800

    Update play_cache.js

--- api/video/play_cache.js
+++ api/video/play_cache.js
@@ -91,10 +91,10 @@ CP.serial(
 				
 		var ip = ips[Math.floor(Math.random() * ips.length)], patt = /([?&]server)=([^#&]*)/i;
 		//if (!ip_cache_uncompleted[ip]) {
-			if (patt.test(url)) url = ('http://'+ ip + url).replace(patt,'$1=' + server_ip);
-			else url = 'http://'+ ip + url + '&server=' + server_ip;
+		//	if (patt.test(url)) url = ('http://'+ ip + url).replace(patt,'$1=' + server_ip);
+		//	else url = 'http://'+ ip + url + '&server=' + server_ip;
 		//} else {
-		//	url = 'http://'+ server_ip + url;
+			url = 'http://'+ server_ip + url;
 		//}
 	//	res.send(url)
 		res.redirect(url);
