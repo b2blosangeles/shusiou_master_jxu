fb63280190a1d869084a81d4d73bc30f97697582
b2blosangeles <b2blosangeles@gmail.com>
2017-11-16 21:26:04 -0800

    Update test_cron_cache_video.js

--- api/test_cron_cache_video.js
+++ api/test_cron_cache_video.js
@@ -32,15 +32,15 @@ _f_s['need_remove']  = function(cbk_s) { /* get database catched local videos */
 		for (var i=0; i < results.length; i++) v[v.length] = results[i].vid;
 		if (v.length) {
 			var str = 'DELETE FROM `video_node` WHERE `vid` IN (' + v.join(',') + ')';
-			//var connection = mysql.createConnection(cfg0);
-			//connection.query(str, function (error, results, fields) {
+			var connection = mysql.createConnection(cfg0);
+			connection.query(str, function (error, results, fields) {
 			//	connection.end();
 			//	if (error) {
 			//		cbk_s(error.message); CP_s.exit = 1;
 			//	} else {
 					cbk_s(str);
 			//	}
-			//});	
+			});	
 		} else {
 			cbk_s(false);
 		}	
