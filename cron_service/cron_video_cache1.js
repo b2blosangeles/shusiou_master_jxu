556b2c7a5af6a29343dd869e052f7f0e86866cb7
b2blosangeles <b2blosangeles@gmail.com>
2017-11-18 16:19:46 -0800

    Update cron_video_cache1.js

--- cron_service/cron_video_cache1.js
+++ cron_service/cron_video_cache1.js
@@ -37,8 +37,8 @@ _f_s['clean_channel_node']  = function(cbk_s) { /* remove offline node  score < 
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 	var str = 'SELECT * FROM `video_node` ' +
-		' WHERE `vid` IN (SELECT  `vid` FROM `video_channel` WHERE `channel` IS  NULL OR `channel` = "" ) ' +
-	    	' AND `node_ip` NOT IN (SELECT `node_ip` FROM `channel` IS NOT NULL AND `channel` <> '') ';
+		' WHERE `vid` IN (SELECT  `vid` FROM `video_channel` WHERE `channel` IS  NOT NULL OR `channel` <> "" ) ' +
+	    	' AND `node_ip` NOT IN (SELECT `node_ip` FROM `channel` IS NOT NULL AND `channel` <> "") ';
 	connection.query(str, function (error, results, fields) {
 		connection.end();
 		cbk_s(results);
