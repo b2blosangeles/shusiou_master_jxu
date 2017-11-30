58f9a16de3a6b4c7590909c33d3eae06eed7c2f2
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 20:27:56 -0800

    Update cron_videoNodeStatus.js

--- cron_service/cron_videoNodeStatus.js
+++ cron_service/cron_videoNodeStatus.js
@@ -1,12 +1,16 @@
 /* ---  This is final version to catch cached video status  between master server and node side */
 var path = require('path');
 var env = {root_path:path.join(__dirname, '../..')};
+
 env.site_path = env.root_path + '/site';
+env.config_path = '/var/qalet_config';
+
+var config = require(env.config_path + '/config.json');
 
 var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
     crowdProcess =  require(env.root_path + '/package/crowdProcess/crowdProcess'),
     request =  require(env.root_path + '/package/request/node_modules/request'),
-    cfg0 = require(env.site_path + '/api/cfg/db.json'); 		
+    cfg0 = config.db; 		
 
 var fs = require('fs');
 var childProcess = require('child_process');
