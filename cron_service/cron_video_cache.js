67b5d9ca6a030afe1d67e3a3cfce2629fd6b4770
b2blosangeles <b2blosangeles@gmail.com>
2017-11-29 20:23:43 -0800

    Update cron_video_cache.js

--- cron_service/cron_video_cache.js
+++ cron_service/cron_video_cache.js
@@ -3,12 +3,16 @@
 var path = require('path');
 var env = {root_path:path.join(__dirname, '../..')};
 env.site_path = env.root_path + '/site';
+env.site_path = env.root_path + '/site';
+env.config_path = '/var/qalet_config';
+
+var config = require(env.config_path + '/config.json');
 
 var default_cnt = 2;
 
 var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
     crowdProcess =  require(env.root_path + '/package/crowdProcess/crowdProcess'),
-    cfg0 = require(env.site_path + '/api/cfg/db.json'),
+    cfg0 = config.db,
     fs = require('fs');
 
 
