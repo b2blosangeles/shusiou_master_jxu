d0309af1463210063465da18e472c2a4dedd448f
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 09:23:36 -0800

    Update embed_new_curriculum.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_new_curriculum.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_new_curriculum.jsx
@@ -33,7 +33,7 @@ try {
 		},
 		bgFilmStyle:function(rec) {
 			var dt = Math.floor((Math.random() * 100) + 1);
-			var url = 'http://' + rec.server_ip + '/api/video/play_stream.api?type=image*=&vid=' + rec.vid + '&s=20&w=90';
+			var url = 'http://' + rec.server_ip + '/api/video/play_stream.api?type=image&vid=' + rec.vid + '&s=20&w=90';
 			console.log('---url---');
 			console.log(url);
 			return {width:'90px', background:'url('+url+')',
