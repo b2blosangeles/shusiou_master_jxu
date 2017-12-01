f31eeddc3167ac85a17a04dd0beb2492da64ada9
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 13:56:38 -0800

    Update embed_new_curriculum.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_new_curriculum.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_new_curriculum.jsx
@@ -23,7 +23,7 @@ try {
 				url: shusiou_config.api_server + '/api/video/myVideo.api?opt=getMyActiveVideos',
 				method: "POST",
 				data: {uid:2, token:'xxxxx'},
-			//	data: me.props.route.env.state.auth,
+				data: me.props.route.env.state.auth,
 				dataType: "JSON"
 			}).done(function( data) {
 				me.setState({list:data.data});
