9496990e5e487e4aabff1d8c0a41db434fc49253
b2blosangeles <b2blosangeles@gmail.com>
2017-12-02 00:12:26 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -5,10 +5,20 @@
 		cfg0 = config.db;	
 		var connection = mysql.createConnection(cfg0);
 		this.getUid = function(callback) {
-			if (req.body.auth && req.body.auth.uid) {
+			if ((req.body.auth) && (req.body.auth.uid)  && (req.body.auth.token)) {
 				connection.connect();
-				connection.end();				
-				callback({isAuth:true, uid:req.body.auth.uid});
+				var str = "SELECT `uid` FROM `auth_session` "+    
+				" WHERE `uid` = '" + req.body.auth.uid + "' AND `token` = '" + req.body.auth.token + "';";
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					if (error) {
+						callback({isAuth:false});
+					} else if (results.length) { 
+						callback({isAuth:true, uid:req.body.auth.uid});
+					} else {
+						callback({isAuth:false});
+					}
+				});  
 			} else {
 				callback({isAuth:false});	
 			}
