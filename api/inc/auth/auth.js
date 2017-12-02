7e5f5b4cf41345ea989d01219a6d6e635471be71
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 21:30:25 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -1,9 +1,13 @@
 (function () { 
 	var obj =  function (connection, post) {
 		this.getUid = function(callback) {
-			connection.connect();
-			connection.end();
-			callback(post);
+			if (post.auth && post.auth.uid) {
+				connection.connect();
+				connection.end();				
+				callback({isAuth:true, uid:post.auth.uid});
+			} else {
+				callback({isAuth:false});	
+			}
 		}	
 	};
 	module.exports = obj;
