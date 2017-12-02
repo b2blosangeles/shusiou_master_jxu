4d37ebe6aabdbf4742b9268f48ebb6f1cea2db9f
b2blosangeles <b2blosangeles@gmail.com>
2017-12-02 09:25:25 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -1,3 +1,4 @@
+/*
 (function () { 
 	var obj =  function (env, pkg, req) {
 		var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
@@ -26,3 +27,29 @@
 	};
 	module.exports = obj;
 })();
+*/
+module.exports =  function (env, pkg, req) {
+		var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+		config = require(env.config_path + '/config.json'),
+		cfg0 = config.db;	
+		var connection = mysql.createConnection(cfg0);
+		this.getUid = function(callback) {
+			if ((req.body.auth) && (req.body.auth.uid)  && (req.body.auth.token)) {
+				connection.connect();
+				var str = "SELECT `uid` FROM `auth_session` "+    
+				" WHERE `uid` = '" + req.body.auth.uid + "' AND `token` = '" + req.body.auth.token + "';";
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					if (error) {
+						callback({isAuth:false});
+					} else if (results.length) { 
+						callback({isAuth:true, uid:req.body.auth.uid});
+					} else {
+						callback({isAuth:false});
+					}
+				});  
+			} else {
+				callback({isAuth:false});	
+			}
+		}	
+	};
