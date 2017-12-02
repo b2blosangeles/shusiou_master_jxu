a09bf3eaec46bbc13fbd6b817475185f3d61a725
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 22:09:28 -0800

    Update my_videos.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_videos.jsx
@@ -19,11 +19,12 @@ try {
 			$.ajax({
 				url: shusiou_config.api_server + '/api/video/myVideo.api?opt=getMyVideos',
 				method: "POST",
-				data: me.props.route.env.state.auth,
+				data: {auth:me.props.parent.props.route.env.state.auth},
 				dataType: "JSON"
 			}).done(function( data) {
-				me.setState({list:data.data});
-				console.log(data);
+				if (data.status == 'success') {
+					me.setState({list:data.data});
+				}	
 			}).fail(function( jqXHR, textStatus ) {
 				console.log('error');
 			});			
