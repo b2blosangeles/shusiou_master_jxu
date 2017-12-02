07942166a00c7676038c621462e070e4eac58f04
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 21:48:12 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -1,5 +1,5 @@
 (function () { 
-	var obj =  function (connection,req) {
+	var obj =  function (req) {
 		var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 		config = require(env.config_path + '/config.json'),
 		cfg0 = config.db;	
