9d3e63d93b4605c8717a11b5b0be46f678d00168
b2blosangeles <b2blosangeles@gmail.com>
2017-12-01 21:47:58 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -2,7 +2,8 @@
 	var obj =  function (connection,req) {
 		var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 		config = require(env.config_path + '/config.json'),
-		cfg0 = config.db;		
+		cfg0 = config.db;	
+		var connection = mysql.createConnection(cfg0);
 		this.getUid = function(callback) {
 			if (req.body.auth && req.body.auth.uid) {
 				connection.connect();
