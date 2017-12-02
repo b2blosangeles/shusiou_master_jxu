13de10563e4498e2080f40045f9dec76e8b80ac6
b2blosangeles <b2blosangeles@gmail.com>
2017-12-02 06:57:12 -0800

    Update myVideo.js

--- api/video/myVideo.js
+++ api/video/myVideo.js
@@ -10,7 +10,7 @@ var AUTH = require(env.site_path + '/api/inc/auth/auth.js'),
     auth = new AUTH(env, pkg, req);
 
 auth.getUid(function(app_data) {
-	app(app_data);	
+	// app(app_data);	
 });
 var app = function(app_data) {
 	var opt = req.query['opt'];
@@ -430,3 +430,4 @@ var app = function(app_data) {
 	}
 }
 
+app();
