15d2a9633ef315b56715e633465603265c4faf1b
b2blosangeles <b2blosangeles@gmail.com>
2017-12-02 16:52:41 -0800

    Update auth.js

--- api/inc/auth/auth.js
+++ api/inc/auth/auth.js
@@ -1,5 +1,5 @@
 (function () { 
-	var obj =  function (env, pkg, req) {
+	var obj =  function (env, pkg, req, app) {
 		var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 		config = require(env.config_path + '/config.json'),
 		cfg0 = config.db;	
