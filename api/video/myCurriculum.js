4b48c92045dd3e19067c532e4949b3ec727f733f
b2blosangeles <b2blosangeles@gmail.com>
2017-12-03 21:42:38 -0800

    Create myCurriculum.js

---
+++ api/video/myCurriculum.js
@@ -0,0 +1,30 @@
+var app = function(auth_data) { 
+	var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+	    config = require(env.config_path + '/config.json'),
+	    cfg0 = config.db;
+	
+	var opt = req.query['opt'];
+	var uid = auth_data.uid;
+	
+	var connection = mysql.createConnection(cfg0);	
+	
+	switch(opt) {
+		case 'add':
+    res.send(({status:'error', message:'Wrong opt value!'})
+			break;
+		default:
+			res.send({status:'error', message:'Wrong opt value!'});
+	}
+};
+
+delete require.cache[env.site_path + '/api/inc/auth/auth.js'];
+var AUTH = require(env.site_path + '/api/inc/auth/auth.js'),
+    auth = new AUTH(env, pkg, req);
+
+auth.getUid(function(auth_data) {
+	if (!auth_data.isAuth || !auth_data.uid) {
+		res.send({status:'failure', message:'Auth failure'});
+	} else {	
+		app(auth_data);
+	}
+});
