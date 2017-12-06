ebe7bd20ac2f0404f9cc70b6720315ccf7b8a965
b2blosangeles <b2blosangeles@gmail.com>
2017-12-06 08:08:09 -0800

    Update my_curriculums.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculums.jsx
@@ -52,6 +52,8 @@ try {
 		},
 		videoImageFilm:function(t, a) {
 			var url = 'http://' + a.server_ip + '/api/video/play_stream.api?type=image&vid='+ a.vid +'&w=180&s='+t;
+			console.log(url);
+			return true;
 			return {width:'100%', background:'url('+url+')',
 				'background-size':'cover'}
 		},		
