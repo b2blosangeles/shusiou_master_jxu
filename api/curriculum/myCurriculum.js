a7070100163ad9888491aa04093a7ecb2697dea4
b2blosangeles <b2blosangeles@gmail.com>
2017-12-07 06:21:01 -0800

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -123,6 +123,63 @@ var app = function(auth_data) {
 				},
 				3000
 			);
+			break;	
+		case 'getCurriculumById':
+			var CP = new pkg.crowdProcess();
+			var _f = {};
+			var curriculum_id = res.body.curriculum_id;
+			_f['S1'] = function(cbk) {
+				var connection = mysql.createConnection(cfg0);
+				connection.connect();
+				var str = 'SELECT A.curriculum_id, B.* FROM `curriculums` A LEFT JOIN  `video` B  ON A.vid = B.vid '+
+				    ' WHERE A.curriculum_id = "' + curriculum_id + '" AND  A.uid = "' + uid + '";';
+
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					if (error) {
+						cbk(error.message);
+						return true;
+					} else {
+						cbk(results);
+					}
+				});  
+			};
+			_f['S2'] = function(cbk) {
+				var vstr = '0';
+				for (var i = 0; i < CP.data.S1.length; i++) {
+					vstr += ',' +  CP.data.S1[i].vid; 
+				}
+
+				var connection = mysql.createConnection(cfg0);
+				connection.connect();
+
+				var str = 'SELECT * FROM  `video_node` WHERE `vid` IN (' + vstr + ')';
+
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					var v = {};
+					if (results.length) {
+						
+						for (var i = 0; i < results.length; i++) {
+							if (!v[results[i].vid]) v[results[i].vid] = [];
+							v[results[i].vid][v[results[i].vid].length] = results[i].node_ip;
+						}
+						cbk(v);
+					} else cbk(v);
+				});  
+			};			
+			CP.serial(
+				_f,
+				function(data) {
+					var d = [];
+					for (var i=0; i <  CP.data.S1.length; i++) {
+						CP.data.S1[i].node_ip = CP.data.S2[CP.data.S1[i].vid];
+						d[d.length] =  CP.data.S1[i];
+					}
+					res.send({_spent_time:data._spent_time, status:data.status, data:d});
+				},
+				3000
+			);
 			break;				
 		default:
 			res.send({status:'error', message:'Wrong opt value!'});
