375a56356d246b0b0e7151d761e3432fdc863f20
b2blosangeles <b2blosangeles@gmail.com>
2017-12-07 08:28:59 -0800

    Update embed_curriculum_demo.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_demo.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_demo.jsx
@@ -23,7 +23,7 @@ try {
 							<p><b>Video ID</b>:{me.props.parent.state.curriculum.vid}</p>  
 							<p><b>Video Length</b>:({me.props.parent.state.video.length} Secs)</p>
 							<img src={shusiou_config.api_server + 
-							'/api/video/play_stream?type:=image&vid='+me.props.parent.state.curriculum.vid+
+							'/api/video/play_stream.api?type=image&vid='+me.props.parent.state.curriculum.vid+
 							'&w=180&s=10'}/></div>	
 					
 					</div>)
