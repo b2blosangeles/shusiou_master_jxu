f01ae2061b5c12c7250224849c57f4d0d6f0d473
b2blosangeles <b2blosangeles@gmail.com>
2017-12-14 22:50:42 -0800

    Update curriculums.js

--- api/curriculum/curriculums.js
+++ api/curriculum/curriculums.js
@@ -9,7 +9,7 @@ switch(req.body.cmd) {
 		var _f = {};
 		_f['S1'] = function(cbk) {
 			var connection = mysql.createConnection(cfg0);
-			var str = 'SELECT A.*, B.vid FROM `curriculums` A LEFT JOIN `video` B  ON A.vid = B.vid WHERE A.published = 1;';
+			var str = 'SELECT A.*, B.vid, B.server_ip FROM `curriculums` A LEFT JOIN `video` B  ON A.vid = B.vid WHERE A.published = 1;';
 			connection.query(str, function (error, results, fields) {
 				if (error) {
 					cbk(error.message);
