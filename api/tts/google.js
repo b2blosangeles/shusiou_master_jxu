6fe6704087c796fe2f3f45eab5abf9838daf8fc2
b2blosangeles <b2blosangeles@gmail.com>
2017-12-17 08:18:27 -0800

    Update google.js

--- api/tts/google.js
+++ api/tts/google.js
@@ -75,11 +75,14 @@ _f['create_cache'] = function(cbk) {
 				'User-Agent': 'stagefright/1.2 (Linux;Android 5.0)'
 			}
 		};
+		cbk(options);
+		/*
 		var p = pkg.request(options, response);		
 		response.pipe(fs.createWriteStream(fn));
 		response.on('end', function() {
 			cbk('niu_true');
 		});
+		*/
 	}).catch(function (err) {
 	   	cbk('err.message');
 	});
