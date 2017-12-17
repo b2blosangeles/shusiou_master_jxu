161c04fabab9ed4dbc3d2af160beb0ee1e593c21
b2blosangeles <b2blosangeles@gmail.com>
2017-12-16 21:52:48 -0800

    Update google.js

--- api/tts/google.js
+++ api/tts/google.js
@@ -117,39 +117,3 @@ CP.serial(
 	},
 	58000
 );
-return true;
-
-var tts_dir = '/var/' + lang + '/';
-var sh = require(env.site_path + '/api/inc/shorthash/node_modules/shorthash');
-var code = sh.unique(str);
-var fn = tts_dir + sh.unique(str)+'.mp3';
-res.send(code);
-return true;
- pkg.fs.stat(fn, function(err, data) {
-     // if (err) {  
-     if (true) { 	     
-	var googleTTS = require(env.site_path + '/api/inc/google-tts-api/node_modules/google-tts-api/');
-	googleTTS(str, lang, 1)   // speed normal = 1 (default), slow = 0.24 
-	.then(function (url) {
-	   var fs = require('fs');
-	   var text = 'Hello World';
-	   var options = {
-	      url: url,
-	      headers: {
-		 'Referer': 'http://translate.google.com/',
-		 'User-Agent': 'stagefright/1.2 (Linux;Android 5.0)'
-	      }
-	   }
-	   var p = pkg.request(options);
-	      p.pipe(fs.createWriteStream(fn));
-	      p.pipe(res);
-	})
-	.catch(function (err) {
-	   res.send(err.stack);
-	});
-     } else { 
-	     res.sendFile(fn);
-	     return true; 
-    }	     
-});
-return true;
