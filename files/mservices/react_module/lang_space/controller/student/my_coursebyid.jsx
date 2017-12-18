2beaa6e53d872b0ca03efb9787024f88bff79bbb
b2blosangeles <b2blosangeles@gmail.com>
2017-12-18 07:32:59 -0800

    Update my_coursebyid.jsx

--- files/mservices/react_module/lang_space/controller/student/my_coursebyid.jsx
+++ files/mservices/react_module/lang_space/controller/student/my_coursebyid.jsx
@@ -13,7 +13,6 @@ try {
 		
 		getCurriculumById: function(curriculum_id, cbk) {
 			var me = this;
-			alert(curriculum_id);
 			me.props.route.env.engine({
 				url: shusiou_config.api_server + '/api/curriculum/curriculums.api',
 				method: "POST",
