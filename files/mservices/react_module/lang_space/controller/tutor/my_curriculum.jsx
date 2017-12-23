6da006d3732443ac6cee158d3f31ff296dd29126
b2blosangeles <b2blosangeles@gmail.com>
2017-12-22 20:27:42 -0800

    Update my_curriculum.jsx

--- files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/my_curriculum.jsx
@@ -193,7 +193,7 @@ try {
 			}
 			
 			me.props.route.env.engine({
-				url: shusiou_config.api_server + '/api/curriculum/myCurriculum.api',
+				url: '/api/curriculum/myCurriculum.api',
 				method: "POST",
 				data: data,
 				dataType: "JSON"
