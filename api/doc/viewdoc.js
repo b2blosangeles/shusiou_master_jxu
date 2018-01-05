2219bfd29d060202eed0c7794f680351fb2ebd35
b2blosangeles <b2blosangeles@gmail.com>
2018-01-04 17:39:07 -0800

    Update viewdoc.js

--- api/doc/viewdoc.js
+++ api/doc/viewdoc.js
@@ -14,8 +14,7 @@ var source = {
 if (!source[req.body.code]) {
 	res.send({title: req.body.code, body:' Undefined document <b>' +req.body.code+ '</b>!!'});
 } else {
-	var fn = 'document/' + req.body.lang + '/' + source[req.body.code].fn,
-	fn0 = 'document/en/' + source[req.body.code].fn;
+	var fn = '/document/' + req.body.lang + '/' + source[req.body.code].fn;
 	
 	pkg.fs.readFile(env.site_contents_path + fn, 'utf8', function(err, contents) {
 		if (err) {
