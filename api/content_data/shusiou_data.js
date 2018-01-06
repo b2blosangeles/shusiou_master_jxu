fdd52d045457fba4bc4a7b66f4fbdabd63f47bd5
b2blosangeles <b2blosangeles@gmail.com>
2018-01-06 08:05:12 -0800

    Update shusiou_data.js

--- api/content_data/shusiou_data.js
+++ api/content_data/shusiou_data.js
@@ -1,2 +1,4 @@
-res.writeHead(200,{'Content-Type':'text/plain'});
-res.send(env.site_contents_path + '/data/cn/home_page/how_i_studied');
+var fn = env.site_contents_path + '/data/cn/home_page/how_i_studied';
+pkg.fs.readFile(fn, 'utf8', function(err, contents) {
+  res.send(contents);
+});
