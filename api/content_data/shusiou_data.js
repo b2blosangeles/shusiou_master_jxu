addee2319d688bb611178504d84f88b76d5dc4fb
b2blosangeles <b2blosangeles@gmail.com>
2018-01-06 08:00:54 -0800

    Update shusiou_data.js

--- api/content_data/shusiou_data.js
+++ api/content_data/shusiou_data.js
@@ -1,2 +1,2 @@
 res.writeHead(200,{'Content-Type':'text/plain'});
-res.sendFile(env.site_contents_path + '/data/cn/home_page/how_i_studied');
+res.send(env.site_contents_path + '/data/cn/home_page/how_i_studied');
