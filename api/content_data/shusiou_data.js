50dc3631d0a19fb854d3520491fa0155ceecb306
b2blosangeles <b2blosangeles@gmail.com>
2018-01-06 12:24:23 -0800

    Update shusiou_data.js

--- api/content_data/shusiou_data.js
+++ api/content_data/shusiou_data.js
@@ -1,13 +1,16 @@
 var CP = new pkg.crowdProcess(), _f = {};
+var data_folder = env.site_contents_path + '/data/';
 var fn = env.site_contents_path + '/data/cn/home_page/how_i_studied';
 
 _f['S0'] = function(cbk) {
-    pkg.fs.readdir(testFolder, (err, files) => {
-      files.forEach(file => {
+    pkg.fs.readdir(data_folder, (err, files) => {
+        /*      
+        files.forEach(file => {
         //console.log(file);
-      });
-        cbk(files)
-    })
+        });
+        */
+      cbk(files)
+    });
 }
 CP.serial(
   _f,
