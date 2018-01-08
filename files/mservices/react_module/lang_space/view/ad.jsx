2adf90d05c5993d15bc046fb7908dce817f1b7c8
b2blosangeles <b2blosangeles@gmail.com>
2018-01-08 11:21:42 -0800

    Update ad.jsx

--- files/mservices/react_module/lang_space/view/ad.jsx
+++ files/mservices/react_module/lang_space/view/ad.jsx
@@ -23,7 +23,8 @@ try {
 			$.ajax({
 				url: '/api/content_data/shusiou_data.api',
 				method: "POST",
-				dataType: "JSON"
+				dataType: "JSON",
+				data:{lang:'en'}
 			}).done(function(data) {
 				me.setState({text:data});
 			}).fail(function( jqXHR, textStatus ) {
