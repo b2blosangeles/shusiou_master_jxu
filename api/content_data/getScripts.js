7b5c023ae7e79ba9e3844e083000319617f0394d
b2blosangeles <b2blosangeles@gmail.com>
2018-01-08 17:10:47 -0800

    Update getScripts.js

--- api/content_data/getScripts.js
+++ api/content_data/getScripts.js
@@ -13,61 +13,8 @@ _f['script'] = function(cbk) {
       }
     });
 }
-/*
-_f['fd'] = function(cbk) {
-    var langs = CP.data.langs;
-    var CP1 = new pkg.crowdProcess(), _f1 = {};
-    for (var i = 0; i < langs.length; i++) {
-        _f1[langs[i]] = (function(i) {
-            var lang_folder = data_folder + langs[i] + '/';
-            return function(cbk1) {
-                pkg.fs.readdir(lang_folder, (err, files) => {
-                    var list = [];
-                   for (var j = 0; j < files.length; j++) {
-                       if (!param_group || param_group.indexOf(files[j]) !== -1) {
-                            list[list.length] = lang_folder + files[j];
-                       }
-                   }
-                    cbk1(list);
-                });
-            }
-        })(i);
-    }    
-    CP1.serial(_f1, function(data) {
-        var list = [];
-        for (var i = 0; i < langs.length; i++) {
-            list = list.concat(data.results[langs[i]])
-        }
-         cbk(list);
-    },1000); 
-}
-_f['fds'] = function(cbk) {
-    var fds = CP.data.fd;
-    var CP1 = new pkg.crowdProcess(), _f1 = {};
-    for (var i = 0; i < fds.length; i++) {
-        _f1[fds[i]] = (function(i) {
-            var folder = fds[i];
-            return function(cbk1) {
-                pkg.fs.readdir(folder, (err, files) => {
-                    var list = [];
-                   for (var j = 0; j < files.length; j++) {
-                       list[list.length] = folder + '/' + files[j];   
-                   }
-                    cbk1(list);
-                });
-            }
-        })(i);        
-    }
-    CP1.serial(_f1, function(data) {
-        var list = [];
-        for (var i = 0; i < fds.length; i++) {
-            list = list.concat(data.results[fds[i]])
-        }
-        cbk(list);
-    },1000);
-}
 _f['contents'] = function(cbk) {
-    var files = CP.data.fds;
+    var files = CP.data.script;
     var CP1 = new pkg.crowdProcess(), _f1 = {};
     for (var i = 0; i < files.length; i++) {
         _f1[files[i]] = (function(i) {
@@ -87,7 +34,7 @@ _f['contents'] = function(cbk) {
         cbk(list);
     },1000);
 }
-*/
+
 CP.serial(
   _f,
   function(data) {
