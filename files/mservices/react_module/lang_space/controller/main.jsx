3fcf41def2743ff004f17844dbb10880d86a5ee3
b2blosangeles <b2blosangeles@gmail.com>
2018-01-11 11:43:21 -0800

    Update main.jsx

--- files/mservices/react_module/lang_space/controller/main.jsx
+++ files/mservices/react_module/lang_space/controller/main.jsx
@@ -2,22 +2,23 @@ try {
 	var shusiou_url = location.protocol+'//'+window.location.hostname;
 	if (shusiou_url !== 'https://shusiou.com') {
 		window.location.href = window.location.href.replace(shusiou_url, 'https://shusiou.com');
+	} else {
+		var viewpoint = $('.'+mapping_data.id);	
+		var { Router,
+			  Route,
+			  browserHistory,
+			  createMemoryHistory,
+			  hashHistory,
+			  IndexRoute,
+			  IndexLink,
+			  Link } = ReactRouter;
+
+		ReactDOM.render(
+			<Root/>
+			,
+			viewpoint[0]
+		);
 	}
-	var viewpoint = $('.'+mapping_data.id);	
-	var { Router,
-		  Route,
-		  browserHistory,
-		  createMemoryHistory,
-		  hashHistory,
-		  IndexRoute,
-		  IndexLink,
-		  Link } = ReactRouter;
-	
-	ReactDOM.render(
-		<Root/>
-		,
-		viewpoint[0]
-	);
 } catch (err) {
 	console.log('err.message===>');
 	console.log(err.message);
