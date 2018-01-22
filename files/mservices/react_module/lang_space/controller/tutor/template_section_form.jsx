edc35afe398c42900d2cd9166a437b5dd4099f56
b2blosangeles <b2blosangeles@gmail.com>
2018-01-21 16:01:00 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -116,7 +116,7 @@ try {
 					  
 					  {v.map(function (m, idx) {
 						return (<li><a href="JavaScript:void(0)">
-							{idx} -  {m} - {idx]}</a></li>);	
+							{idx} -  {m} - {idx}</a></li>);	
 					})}					  
 				  </ul>
 				</div>	
