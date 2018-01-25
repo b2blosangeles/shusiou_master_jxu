b4d8af4b04cecbd71f0db1fa012cd7eb3455c6c5
b2blosangeles <b2blosangeles@gmail.com>
2018-01-25 08:14:01 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -160,7 +160,19 @@ try {
 			let me = this;
 			let variables = me.state.c_tpl.variables;
 			if (variables) {
-				return (<span>					
+				return (<span>	
+					<table width="100%" className="section_template_frame">	
+						<tr className=""><td>
+							<div className="container-fluid" style={{padding:'6px', 'text-align':'center'}}>
+								{(function() {
+									if (me.props.parent.state.section.id != 'new') return (<button className="btn btn-danger" 
+									onClick={me.props.parent.deleteSection.bind(me, me.props.parent.state.section.id)}>Delete This Section</button>)
+								})()}	
+								<button className="btn btn-default pull-left" onClick={me.props.parent.abortSection.bind(me)}>Abort Change</button>
+								<button className="btn btn-info pull-right" onClick={me.props.parent.acceptSection.bind(me, me.state.c_section)}>Save</button>
+							</div>
+						</td></tr>	
+					</table>						
 					{variables.map(function(v) {
 						switch(v) {
 							case '$section':
@@ -181,18 +193,7 @@ try {
 						}
 
 					})}
-					<table width="100%" className="section_template_frame">	
-						<tr className=""><td>
-							<div className="container-fluid" style={{padding:'6px', 'text-align':'center'}}>
-								{(function() {
-									if (me.props.parent.state.section.id != 'new') return (<button className="btn btn-danger" 
-									onClick={me.props.parent.deleteSection.bind(me, me.props.parent.state.section.id)}>Delete This Section</button>)
-								})()}	
-								<button className="btn btn-default pull-left" onClick={me.props.parent.abortSection.bind(me)}>Abort Change</button>
-								<button className="btn btn-info pull-right" onClick={me.props.parent.acceptSection.bind(me, me.state.c_section)}>Save</button>
-							</div>
-						</td></tr>	
-					</table>						
+						
 					</span>)
 			} else {
 				return (<span>select a script</span>)
