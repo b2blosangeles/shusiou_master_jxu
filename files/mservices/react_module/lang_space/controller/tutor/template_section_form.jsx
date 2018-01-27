6bfc07235bebe71bd19a330aafd845dcbb705c53
b2blosangeles <b2blosangeles@gmail.com>
2018-01-26 16:50:12 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -120,9 +120,9 @@ try {
 		},
 		acceptSection: function() {
 			let me = this;
-			let data = {section_id:me.state.section_id, tpl:me.state.c_tpl, data:me.state.data};
+			let data = {section_id:me.props.section_id, tpl:me.state.c_tpl, data:me.state.data};
 			alert(JSON.stringify(data));
-			alert('AAA- me.props.parent.acceptSection.bind(me, me.state.c_section)');
+			alert('BBC- me.props.parent.acceptSection.bind(me, me.state.c_section)');
 		},
 		
 		templateSelectScript: function() {
