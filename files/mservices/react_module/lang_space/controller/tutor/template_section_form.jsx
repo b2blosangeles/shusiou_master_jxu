f804f1c5d4c97187f1e014e8e8431601baa0d11f
b2blosangeles <b2blosangeles@gmail.com>
2018-01-27 16:03:03 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -94,7 +94,7 @@ try {
 					me.setState({ModalPlus:{type:'popup',  hold:0,
 						box_style:{top:'28px'},
 						title: (<span>Select video section</span>),
-						message: (<Embed_video_editor parent={me.props.parent} curriculum={curriculum} params={params} popid={new Date().getTime()} />),
+						message: (<Embed_video_editor parent={me} curriculum={curriculum} params={params} popid={new Date().getTime()} />),
 						header:false,
 						footer:(<span/>)
 					}});					
@@ -105,7 +105,7 @@ try {
 			me.setState({ModalPlus:{type:'popup',  hold:0,
 				box_style:{top:'28px'},
 				title: (<span>Video Editor</span>),
-				message: (<Embed_video_editor parent={me.props.parent} curriculum={curriculum}  params={params} popid={new Date().getTime()} />),
+				message: (<Embed_video_editor parent={me} curriculum={curriculum}  params={params} popid={new Date().getTime()} />),
 				header:false,
 				footer:(<span/>)
 			}});			
