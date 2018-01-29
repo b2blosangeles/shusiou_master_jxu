b3a57db960cdbc3f0f9b4ce07cd5e09fc26928d4
b2blosangeles <b2blosangeles@gmail.com>
2018-01-29 07:10:05 -0800

    Update getScripts.js

--- api/content_data/getScripts.js
+++ api/content_data/getScripts.js
@@ -68,7 +68,7 @@ _f['contents'] = function(cbk) {
                         let c = contents.replace(/(\n|\r)+$/, '');
                         for (var j = 0; j < patt.length; j++) {
                             var patt_j = new RegExp(patt[j]);
-                            if (patt_j.test(c) {
+                            if (patt_j.test(c)) {
                                 patt_result[patt_result.length] = patt[j];
                             }
                         }
