0642af246a2e66c81a36eb6959fdbf9a19e5fc18
b2blosangeles <b2blosangeles@gmail.com>
2018-01-29 07:37:59 -0800

    Update getScripts.js

--- api/content_data/getScripts.js
+++ api/content_data/getScripts.js
@@ -67,7 +67,8 @@ _f['contents'] = function(cbk) {
                     try {
                         let c = contents.replace(/(\n|\r)+$/, '');
                         for (var j = 0; j < patt.length; j++) {
-                            var patt_j = new RegExp('$'+patt[j], 'g');
+                            let dola_patt = '$'+patt[j];
+                            var patt_j = new RegExp(dola_patt);
                             if (patt_j.test(c)) {
                                 patt_result[patt_result.length] = patt[j];
                             }
