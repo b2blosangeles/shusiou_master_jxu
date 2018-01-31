19ac081867555c95aa70b81e360547c33d647bf4
b2blosangeles <b2blosangeles@gmail.com>
2018-01-31 15:32:04 -0800

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -65,7 +65,8 @@ var app = function(auth_data) {
 				var str = 'INSERT INTO  `curriculum_sections` (`curriculum_id`,`type`,`script`, `created`) VALUES ("' +
 				curriculum_id + '",' +
 				'"niuA",' +
-				'"' + encodeURIComponent(JSON.stringify(req.body.data.section)) + '",' +
+				"'" + JSON.stringify(req.body.data.section) + "'," +
+				//'"' + encodeURIComponent(JSON.stringify(req.body.data.section)) + '",' +
 				'NOW()' +	
 				'); ';
 				var connection = mysql.createConnection(cfg0);
