35e2d497c8e7bc696cc7c7bbeb028908d7362e48
b2blosangeles <b2blosangeles@gmail.com>
2018-01-31 08:32:29 -0800

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -101,7 +101,7 @@ var app = function(auth_data) {
 			CP.serial(
 				_f,
 				function(data) {
-					res.send({_spent_time:data._spent_time, status:data.status, data:data});
+					res.send({_spent_time:data._spent_time, status:'success', data:data});
 				},
 				30000
 			);
