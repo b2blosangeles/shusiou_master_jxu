25a59d018bf1001383bbc697a2d5da2bfe2247ad
b2blosangeles <b2blosangeles@gmail.com>
2018-01-31 08:07:55 -0800

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -12,6 +12,7 @@ var app = function(auth_data) {
 		case 'save':
 			res.send({status:'success', data:req.body.data.curriculum_id});
 			break;
+			/*
 			let curriculum_id = req.body.data.curriculum_id;
 			var CP = new pkg.crowdProcess();
 			var _f = {};
@@ -37,7 +38,7 @@ var app = function(auth_data) {
 				});  
 			};			
 			
-			/*
+			
 			_f['S2'] = function(cbk) {
 				var str = 'DELETE FROM  `curriculum_sections` WHERE `curriculum_id` = "' + req.body.curriculum_id + '"; ';
 				var connection = mysql.createConnection(cfg0);
@@ -96,7 +97,7 @@ var app = function(auth_data) {
 					}
 				});  
 			};
-			*/
+			
 			CP.serial(
 				_f,
 				function(data) {
@@ -104,7 +105,8 @@ var app = function(auth_data) {
 				},
 				30000
 			);
-			break;						
+			break;	
+			*/
 		case 'getList':
 			var CP = new pkg.crowdProcess();
 			var _f = {};
