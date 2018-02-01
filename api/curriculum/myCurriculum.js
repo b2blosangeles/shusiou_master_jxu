b7eeef943e8e711adf2c647d297c8781656a63f3
b2blosangeles <b2blosangeles@gmail.com>
2018-02-01 14:19:47 -0800

    Update myCurriculum.js

--- api/curriculum/myCurriculum.js
+++ api/curriculum/myCurriculum.js
@@ -362,8 +362,7 @@ var app = function(auth_data) {
 				_f,
 				function(data) {
 					try {
-						CP.data.S1.script = [];
-							// JSON.parse(decodeURIComponent(CP.data.S1.script));
+						CP.data.S1.script = JSON.parse(decodeURIComponent(CP.data.S1.script));
 					} catch (err) {
 					};	
 					CP.data.S1.node_ip = CP.data.S2;
