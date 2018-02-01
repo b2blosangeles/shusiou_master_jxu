11b586a5e5851eeea41a64f2f0a35874b7a4e8a1
b2blosangeles <b2blosangeles@gmail.com>
2018-02-01 14:58:35 -0800

    Update embed_curriculum_profile.jsx

--- files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_profile.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/embed_curriculum_profile.jsx
@@ -108,7 +108,7 @@ try {
 									onClick={me.props.parent.editSection.bind(me, rec.id)}>
 									<i className="fa fa-pencil" aria-hidden="true"></i>	
 								</button>
-								{rec.data.s} 
+								{rec.data.answer} 
 							</td>
 						      </tr>)})}		    
 						    </tbody>
