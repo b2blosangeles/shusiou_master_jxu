ded14f123f7623836076a6696db1b3bf06ac702f
b2blosangeles <b2blosangeles@gmail.com>
2018-02-02 13:04:22 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -225,7 +225,7 @@ try {
 		tplSection : function (rec) {
 			let me = this;
 			if (me.state.c_tpl.variables) {
-				return (<span>===niu</span>);
+				return (<span>===niu=={JOSN.stringify(me.state.data)}</span>);
 				return (
 					<span>		
 					{me.state.c_tpl.variables.map(function(v) {
