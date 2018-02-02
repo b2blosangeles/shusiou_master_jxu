bf89b5a4100e2d283e30115cd593739bb4fc59f5
b2blosangeles <b2blosangeles@gmail.com>
2018-02-02 12:07:52 -0800

    Update template_section_form.jsx

--- files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
+++ files/mservices/react_module/lang_space/controller/tutor/template_section_form.jsx
@@ -82,7 +82,7 @@ try {
 				console.log('JSON.stringify(me.props.section)-====->');
 				console.log(me.props.section);
 				me.setState({c_tpl:(me.props.section.tpl)?me.props.section.tpl:data, 
-					section:me.props.section});
+					data:me.props.section.data});
 			}	
 		},
 		popupEditVideo: function(track) {
