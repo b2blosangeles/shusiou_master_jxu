27a3d61c739f51e5500b01485bd3a083ea97fe26
b2blosangeles <b2blosangeles@gmail.com>
2018-02-09 07:34:33 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -1,11 +1,42 @@
 (function () { 
 	var obj =  function (env) {
-		this.queryIP = function(v) {
+		this.sendNamedIP = function(v, req, res) {
 			let me = this;
 			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 	    		config = require(env.config_path + '/config.json'),
 	    		cfg0 = config.db;
-			return '192.241.135.149';
+			/*
+			let ips = [];
+			var str = 'SELECT `node_ip` from `cloud_node` ORDER BY score ASC';
+			var connection = mysql.createConnection(cfg0);
+			connection.connect();
+			connection.query(str, function (error, results, fields) {
+				connection.end();
+				if (error) {
+					cbk(error.message);
+					return true;
+				} else {
+					if (results) {
+						cbk(queryStringToJSON(results[0].script, []));
+					} else {
+						cbk(false);
+					}
+
+				}
+
+			});  			
+			*/
+			me.send([{ 
+				name: v,
+				type: 'A',
+				class: 'IN',
+				ttl: 60,
+				data: '192.241.135.159'
+			}], req, res);
+		};
+		this.send = function(v, req, res) {
+			res.answer = v;	
+			res.end();
 		};
 		this.sendRecord = function(req, res) {
 			let me = this, question = req.question[0], patt = /^IP\_([0-9\_]+)\.service\./ig, m;
@@ -13,23 +44,16 @@
 			
 			if ((m) && (m[1])) {
 				let ip = m[1].replace(/\_/ig, '.');
-				res.answer = [{ 
+				me.send([{ 
 					name: question.name,
 					type: 'A',
 					class: 'IN',
 					ttl: 50,
 					data: ip
-				}];				
+				}], req, res);				
 			} else {
-				res.answer = [{ 
-					name: question.name,
-					type: 'A',
-					class: 'IN',
-					ttl: 50,
-					data: me.queryIP()
-				}];
+				me.sendNamedIP(question.name, req, res);
 			}
-			res.end();
 		};	
 	};
 	module.exports = obj;
