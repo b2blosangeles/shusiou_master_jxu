2cf2d9c66ae4c5011b66984b354b72dddf03ef1d
b2blosangeles <b2blosangeles@gmail.com>
2018-02-08 16:57:39 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -3,6 +3,14 @@
 		this.sendRecord = function(req, res) {
 			let question = req.question[0];
 			//if(question.type == 'A') {
+			res.answer = [{ 
+				name: question.name,
+				type: 'A',
+				class: 'IN',
+				ttl: 60,
+				data: '192.241.135.140'
+				}];
+			/*
 			    res.answer.push({ 
 				name: question.name,
 				type: 'A',
@@ -10,6 +18,7 @@
 				ttl: 600,
 				data: '192.241.135.141'
 				});
+			*/	
 			 // }
 			//  res.end();
 				res.end();
