071311789fdd7d71b8ff160b4be27018a14720c7
b2blosangeles <b2blosangeles@gmail.com>
2018-02-08 16:39:28 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -3,9 +3,10 @@
 		this..sendRecord = function(req, res) {
 			let question = req.question[0];
 			//if(question.type == 'A') {
-			    res.answer.push({name:question.name, type:'A', data:"192.241.135.139", 'ttl':60});
+			 //   res.answer.push({name:question.name, type:'A', data:"192.241.135.139", 'ttl':60});
 			 // }
-			  res.end();			
+			//  res.end();
+				res.end("192.241.135.139");
 		 	// return '192.241.135.143';
 		};	
 	};
