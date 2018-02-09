16b787f493688a9332e4127a446035663463a4cd
b2blosangeles <b2blosangeles@gmail.com>
2018-02-08 16:19:26 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -1,7 +1,7 @@
 (function () { 
 	var obj =  function () {
 		this.getIpByName = function(dns_question) {
-		  return '192.241.135.145';
+		  return '192.241.135.143';
 		};	
 	};
 	module.exports = obj;
