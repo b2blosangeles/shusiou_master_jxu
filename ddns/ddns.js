ddcc8a06684f9e249efbbe2fa17f5430b7a09ef8
b2blosangeles <b2blosangeles@gmail.com>
2018-02-08 22:25:17 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -1,11 +1,11 @@
 (function () { 
 	var obj =  function (env) {
-		this.queryRecord = function() {
+		this.queryRecord = function(v) {
 			let me = this;
 			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 	    		config = require(env.config_path + '/config.json'),
 	    		cfg0 = config.db;
-			return false;
+			return true;
 		};
 		this.sendRecord = function(req, res) {
 			let me = this, question = req.question[0], patt = /^IP\_([0-9\_]+)\.service\./ig, m;
