8b8e8c78128a8057c6ff0cb8a287006d30068a31
b2blosangeles <b2blosangeles@gmail.com>
2018-02-08 22:30:50 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -1,11 +1,11 @@
 (function () { 
 	var obj =  function (env) {
-		this.queryRecord = function(v) {
+		this.queryIP = function(v) {
 			let me = this;
 			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 	    		config = require(env.config_path + '/config.json'),
 	    		cfg0 = config.db;
-			return true;
+			return '192.241.135.149';
 		};
 		this.sendRecord = function(req, res) {
 			let me = this, question = req.question[0], patt = /^IP\_([0-9\_]+)\.service\./ig, m;
@@ -26,7 +26,7 @@
 					type: 'A',
 					class: 'IN',
 					ttl: 50,
-					data: (me.queryRecord())?'192.241.135.143':'192.241.135.146'
+					data: me.queryIP()
 				}];
 			}
 			res.end();
