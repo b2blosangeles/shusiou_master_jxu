7a14eb60dec5247346a5646973efbe7d3369cff1
b2blosangeles <b2blosangeles@gmail.com>
2018-02-10 20:50:22 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -6,34 +6,46 @@
   		}		
 		this.sendNamedIP = function(name, key, req, res) {
 			let me = this;
-			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-	    		config = require(env.config_path + '/config.json'),
-	    		cfg0 = config.db;
-			let ips = [];
-			var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
-			var connection = mysql.createConnection(cfg0);
-			connection.connect();
-			connection.query(str, function (error, results, fields) {
-				connection.end();
-				if (error) {
-					return true;
-				} else {
-					if (results) {
-						for (var i = 0; i < results.length; i++) {
-							ips[ips.length] =  results[i].node_ip;
-						}
+			if (!env.dns_matrix) {
+				var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+				config = require(env.config_path + '/config.json'),
+				cfg0 = config.db;
+				let ips = [];
+				var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
+				var connection = mysql.createConnection(cfg0);
+				connection.connect();
+				connection.query(str, function (error, results, fields) {
+					connection.end();
+					if (error) {
+						return true;
 					} else {
-					}
+						if (results) {
+							for (var i = 0; i < results.length; i++) {
+								ips[ips.length] =  results[i].node_ip;
+							}
+						} else {
+						}
 
-				}
+					}
+					env.dns_matrix = ips;
+					me.send([{ 
+						name: name,
+						type: 'A',
+						class: 'IN',
+						ttl: 60,
+						data: ips[key]
+					}], req, res);
+				});
+			} else {
+				console.log(env.dns_matrix);
 				me.send([{ 
 					name: name,
 					type: 'A',
 					class: 'IN',
 					ttl: 60,
-					data: ips[key]
-				}], req, res);
-			});  			
+					data: env.dns_matrix[key]
+				}], req, res);			
+			}
 		};
 		this.send = function(v, req, res) {
 			let me = this;
