a081cfd11b29ae99b9140fb4fcc299063bdf00a3
b2blosangeles <b2blosangeles@gmail.com>
2018-02-10 20:10:12 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -60,6 +60,7 @@
 					console.log(m[1].replace(/\_/ig, '.'));
 					console.log((me.validateIPaddress(m[1].replace(/\_/ig, '.'));
 					return true;
+						     /*
 					if (me.validateIPaddress(m[1].replace(/\_/ig, '.'))) {
 						me.send([{ 
 							name: question.name,
@@ -78,7 +79,7 @@
 						}], req, res);							
 					}
 					break;
-			
+			*/
 					break;
 				case 'idx': 
 					m = new RegExp(patt[mh]).exec(question.name);
