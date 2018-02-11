15e5b5ae975e43939f9a5b85185617f3756aea03
b2blosangeles <b2blosangeles@gmail.com>
2018-02-10 21:34:54 -0800

    Update setupDomainMatrix.js

--- api/setupDomainMatrix.js
+++ api/setupDomainMatrix.js
@@ -1,3 +1,23 @@
-//env.dns_matrix = ["192.241.135.150","192.241.135.150","198.199.120.150"];
-//delete env.dns_matrix
-res.send(env);
+var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+config = require(env.config_path + '/config.json'),
+cfg0 = config.db;
+let ips = [];
+var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
+var connection = mysql.createConnection(cfg0);
+connection.connect();
+connection.query(str, function (error, results, fields) {
+  connection.end();
+  if (error) {
+    return true;
+  } else {
+    if (results) {
+      for (var i = 0; i < results.length; i++) {
+        ips[ips.length] =  results[i].node_ip;
+      }
+    } else {
+    }
+
+  }
+  env.dns_matrix = ips;
+  res.send(env);
+});
