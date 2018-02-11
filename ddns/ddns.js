cda6c819b9102abd200adce246edf34b2a52379e
b2blosangeles <b2blosangeles@gmail.com>
2018-02-10 16:49:57 -0800

    Update ddns.js

--- ddns/ddns.js
+++ ddns/ddns.js
@@ -2,46 +2,34 @@
 	var obj =  function (env, ns_ip) {
 		this.sendNamedIP = function(name, key, req, res) {
 			let me = this;
-			if (!env.ddns) {
-				var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-				config = require(env.config_path + '/config.json'),
-				cfg0 = config.db;
-				let ips = [];
-				var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
-				var connection = mysql.createConnection(cfg0);
-				connection.connect();
-				connection.query(str, function (error, results, fields) {
-					connection.end();
-					if (error) {
-						return true;
-					} else {
-						if (results) {
-							for (var i = 0; i < results.length; i++) {
-								ips[ips.length] =  results[i].node_ip;
-							}
-						} else {
+			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+	    		config = require(env.config_path + '/config.json'),
+	    		cfg0 = config.db;
+			let ips = [];
+			var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
+			var connection = mysql.createConnection(cfg0);
+			connection.connect();
+			connection.query(str, function (error, results, fields) {
+				connection.end();
+				if (error) {
+					return true;
+				} else {
+					if (results) {
+						for (var i = 0; i < results.length; i++) {
+							ips[ips.length] =  results[i].node_ip;
 						}
+					} else {
+					}
 
-					}
-					env.ddns = ips;
-					me.send([{ 
-						name: name,
-						type: 'A',
-						class: 'IN',
-						ttl: 60,
-						data: ips[key]
-					}], req, res);
-					}
-				}); 
-			} else {
+				}
 				me.send([{ 
 					name: name,
 					type: 'A',
 					class: 'IN',
 					ttl: 60,
-					data: env.ddns[key]
-				}], req, res);				
-			}
+					data: ips[key]
+				}], req, res);
+			});  			
 		};
 		this.send = function(v, req, res) {
 			res.answer = v;	
