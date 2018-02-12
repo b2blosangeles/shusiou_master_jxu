5aafe98bf4583da575ac2247a9317d1f70c09d20
b2blosangeles <b2blosangeles@gmail.com>
2018-02-12 14:07:55 -0800

    Update admin.js

--- api/admin.js
+++ api/admin.js
@@ -21,7 +21,7 @@ switch(opt) {
         break;	
     case 'git_all_pull':
 	pkg.fs.exists('/var/cert/', function(exists) {
-		let cmd_plus = (exists)?' && cd /var/cert/ && git pull ':'';
+		let cmd_plus = (exists)?' && cd /var/cert && git pull ':'';
 		var cmd = 'cd ' + env.site_path + '&& git pull && cd ' + env.site_contents_path + '&& git pull cd ' + env.root_path + '&& git pull' + cmd_plus; 	
 		pkg.exec(cmd, function(error, stdout, stderr) {
 			 res.send(cmd);
