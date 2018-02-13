6d0f6c5d2552972eb98cb7a8b80fe313af9f1a39
b2blosangeles <b2blosangeles@gmail.com>
2018-02-12 16:51:40 -0800

    Update get_default_ad.js

--- api/ad/get_default_ad.js
+++ api/ad/get_default_ad.js
@@ -4,7 +4,22 @@ var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 
 var CP = new pkg.crowdProcess();
 var _f = {};
+_f['P0'] = function(cbk) {
+	var connection = mysql.createConnection(cfg0);
+	connection.connect();
 
+	var str = 'SELECT A.*, B.`node_ip` FROM  `video` A LEFT JOIN `video_node` B ON A.`vid` = B.`vid` WHERE 1 ORDER BY A.`uploaded` DESC LIMIT 3';
+
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (error) {
+			cbk(error.message);
+			return true;
+		} else {
+			cbk(results);
+		}
+	});  
+};
 _f['Q0'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
