cbc7f92e3e36103165c4ad7cd6f098fa74c03445
b2blosangeles <b2blosangeles@gmail.com>
2018-02-13 07:19:41 -0800

    Update checkDomainMatrix.js

--- api/checkDomainMatrix.js
+++ api/checkDomainMatrix.js
@@ -1,28 +1,9 @@
 
-var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-config = require(env.config_path + '/config.json'),
-cfg0 = config.db;
-let ips = [];
-var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
-var connection = mysql.createConnection(cfg0);
-connection.connect();
-connection.query(str, function (error, results, fields) {
-  connection.end();
-  if (error) {
-    return true;
-  } else {
-    if (results) {
-      for (var i = 0; i < results.length; i++) {
-        ips[ips.length] =  results[i].node_ip;
-      }
-    } else {
-    }
-
-  }
+  let ips = env.matrix;
   
   let mp = {};
   for (o in ips) {
-    mp[ips[o]] = parseInt(o) + 1; 
+    mp[ips[o]] = parseInt(o) + 2; 
   }
   
   res.send(mp);
