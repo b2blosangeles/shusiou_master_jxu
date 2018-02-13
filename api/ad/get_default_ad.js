99a4405280edd8f9b432e0edb2d1060ae53c5d39
b2blosangeles <b2blosangeles@gmail.com>
2018-02-12 16:52:58 -0800

    Update get_default_ad.js

--- api/ad/get_default_ad.js
+++ api/ad/get_default_ad.js
@@ -5,20 +5,7 @@ var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 var CP = new pkg.crowdProcess();
 var _f = {};
 _f['P0'] = function(cbk) {
-	var connection = mysql.createConnection(cfg0);
-	connection.connect();
-
-	var str = 'SELECT A.*, B.`node_ip` FROM  `video` A LEFT JOIN `video_node` B ON A.`vid` = B.`vid` WHERE 1 ORDER BY A.`uploaded` DESC LIMIT 3';
-
-	connection.query(str, function (error, results, fields) {
-		connection.end();
-		if (error) {
-			cbk(error.message);
-			return true;
-		} else {
-			cbk(results);
-		}
-	});  
+	cbk(env);  
 };
 _f['Q0'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
@@ -44,7 +31,7 @@ CP.serial(
 		for (o in data.results.Q0) {
 			list[list.length] = data.results.Q0[o];
 		}
-		res.send({_spent_time:data._spent_time, status:data.status, data:list});
+		res.send({_spent_time:data._spent_time, status:data.status, data:list, env:env});
 	},
 	6000
 );
