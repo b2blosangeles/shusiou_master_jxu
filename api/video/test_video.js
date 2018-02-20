434e859f12d5f8914661ed488602b4b6e56e20fe
b2blosangeles <b2blosangeles@gmail.com>
2018-02-20 06:46:37 -0800

    Update test_video.js

--- api/video/test_video.js
+++ api/video/test_video.js
@@ -24,45 +24,18 @@ switch(type) {
 			if (err) {  write404(file_video + ' does not exist'); }
 			else {
 				if (cache_only)	{
-					var file = pkg.fs.createReadStream(file_video);
+					var file = pkg.fs.createReadStream('/var/img/niu.mp4');
 					file.pipe(res);
 					var t = new Date().getTime();
-					var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-					cfg0 =  config.db;	
 					var had_error = '';
 					file.on('error', function(err){
 						had_error = '1';
 					});
 
-					file.on('close', function(){
-						var connection = mysql.createConnection(cfg0);
-						connection.connect();
-						var running_time = new Date().getTime() - t;
-						var str = "INSERT INTO `master_node_log` (`type`, `url`, `is_error`, `running_time`, `finished`) VALUES "+    
-						 " ('" + 'video' + "', '" + req.url + "', '" + had_error + "', '" + running_time + "', NOW()) ";
-						connection.query(str, function (error, results, fields) {
-							connection.end();	
-						}); 									
+					file.on('close', function(){ 									
 					});	
 					
 					
-				} else {				
-					var total = data1.size;
-					var range = req.headers.range;
-					if (range) {
-						var parts = range.replace(/bytes=/, "").split("-");
-						var partialstart = parts[0]; var partialend;
-						  partialend =  parts[1];
-						var start = parseInt(partialstart, 10);
-						var end = partialend ? parseInt(partialend, 10) : total-1;
-						var chunksize = (end-start)+1;
-						var file = pkg.fs.createReadStream(file_video, {start:start, end:end});
-						res.writeHead(206, {'Content-Range': 'bytes ' + start + '-' + end + '/' + total, 
-							'Accept-Ranges': 'bytes', 'Content-Length': chunksize, 'Content-Type': 'video/mp4' });
-					       file.pipe(res);
-					} else {
-						res.send('Need streaming player');
-					}
 				}
 			}
 		});
