0bc20bdbc7c701e251339528bc1d792b4ddce3f7
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 15:46:20 -0800

    Update track_aws.js

--- api/video/track_aws.js
+++ api/video/track_aws.js
@@ -1,9 +1,13 @@
+let mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+    config = require(env.config_path + '/config.json'),
+    cfg0 = config.db;
+
 let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
     cfg = {
       id:'shusiou-d-01',
-      endpoint : 'nyc3.digitaloceanspaces.com',
-      accessKeyId: 'QYF3ENCI4TEDFDWFBS6N',
-      secretAccessKey: '7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI'
+      endpoint : 'nyc3.digitaloceanspaces.comA',
+      accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
+      secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
     };
 
 var splitVideo = new awsS3Video(cfg);	
@@ -12,9 +16,4 @@ splitVideo.split('_type', '_file',
         res.send(data);
       }
   );
-/*
-    "objectSpaceDigitalOcean":{
-        "accessKeyId": "QYF3ENCI4TEDFDWFBS6N",
-        "secretAccessKey": "7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI"    
-    }
-    */
+
