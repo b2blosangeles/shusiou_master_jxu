fcfd2b796b632bb68a35b29ba1193879fc3febad
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 15:19:31 -0800

    Update track_aws.js

--- api/video/track_aws.js
+++ api/video/track_aws.js
@@ -3,11 +3,11 @@ let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
       id:'shusiou-d-01',
       endpoint : 'nyc3.digitaloceanspaces.com',
       accessKeyId: 'QYF3ENCI4TEDFDWFBS6N',
-      secretAccessKey: '7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI'	
-  };
+      secretAccessKey: '7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI'
+    };
 
-  var splitVideo = new awsS3Video(cfg);	
-  splitVideo.split('_type', '_file', 
+var splitVideo = new awsS3Video(cfg);	
+splitVideo.split('_type', '_file', 
       function(data) {
         res.send(data);
       }
