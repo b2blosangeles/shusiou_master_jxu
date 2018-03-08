5ed002f3691a3d498c6d0f10d711590a3a12c20b
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 15:15:51 -0800

    Create track_aws.js

---
+++ api/video/track_aws.js
@@ -0,0 +1,14 @@
+  let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
+  cfg = {
+    id:'shusiou-d-01',
+    endpoint : 'nyc3.digitaloceanspaces.com',
+    accessKeyId: 'QYF3ENCI4TEDFDWFBS6N',
+    secretAccessKey: '7DJD8b9iAqD5qsLgRZH9OXfgOQMob/edWouwiqYeOwI'	
+  };
+
+  var splitVideo = new awsS3Video(cfg);	
+  splitVideo.split(
+      function(data) {
+        res.send(data);
+      }
+  );
