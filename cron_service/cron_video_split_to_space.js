8f8ba855b5848889a7f7f1405c0d84bbbfc2aa49
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:15:10 -0800

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -7,20 +7,12 @@ env.site_path = env.root_path + '/site';
 env.config_path = '/var/qalet_config';
 
 var config = require(env.config_path + '/config.json');
-
-var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-    crowdProcess =  require(env.root_path + '/package/crowdProcess/crowdProcess'),
-    cfg0 = config.db,
-    fs = require('fs');
-
-let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
-    cfg = {
+let cfg = {
       id:'shusiou-d-01',
       endpoint : 'nyc3.digitaloceanspaces.com',
       accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
       secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
     };
-
 var splitVideo = new awsS3Video(cfg, env);	
 splitVideo.split('_t', '/var/img/video.mp4', 
       function(data) {
