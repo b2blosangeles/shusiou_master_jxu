56b0458b93b43a7e456ce7e2eec5e5cb68c86475
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:48:00 -0800

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -1,5 +1,22 @@
 (function () { 
 	var obj =  function (config, env, pkg) {
+		
+		this.split = function(_type, _file, _cbk) {
+			let me = this;
+			let _p = _file.match(/(.+)\/([^\/]+)$/);
+			me.source_path = _p[1] + '/';
+			me.source_file = _p[2];
+			me.tmp_folder = '/var/shusiou_cache/tmpvideo/' + source_file + '/' + _type + '/';
+			me.space_url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/';
+			me.space_info = 'shusiou/' + source_file + '/_info.txt';
+			me.space_dir = 'shusiou/' + source_file + '/' + _type + '/';
+			me.trunkSize = 1024 * 1024;			
+			
+			var CP = new pkg.crowdProcess();
+			var _f = {}; 
+			
+			_cbk('==niu==2=');
+		};
 		this.init = function() {
 			let me = this;
 			const AWS = require(env.site_path + '/api/inc/aws-sdk/node_modules/aws-sdk')
@@ -7,24 +24,24 @@
 			    endpoint: new AWS.Endpoint('nyc3.digitaloceanspaces.com'),
 			    accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
 			    secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
-			});		
+			});
+			me.space_id = 'shusiou-d-01',
 		}
-		this.split = function(_type, _file, _cbk) {
-			let _p = _file.match(/(.+)\/([^\/]+)$/);
-			let source_path = _p[1] + '/',
-			    source_file = _p[2],
-			    tmp_folder = '/var/shusiou_cache/tmpvideo/' + source_file + '/' + _type + '/',
-			    space_id = 'shusiou-d-01',
-			    space_url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/', 
-			    space_info = 'shusiou/' + source_file + '/_info.txt',
-			    space_dir = 'shusiou/' + source_file + '/' + _type + '/',
-			    trunkSize = 1024 * 1024;			
-			
-			var CP = new pkg.crowdProcess();
-			var _f = {}; 
-			
-			_cbk('==niu==2=');
-		};
+		this.writeInfo = function(v, cbk) {
+			let me = this,
+			    params = {
+				Body: JSON.stringify(v),
+				Bucket: me.space_id,
+				Key: me.space_info,
+				ContentType: 'text/plain',
+				ACL: 'public-read'
+			};	
+
+			me.s3.putObject(params, function(err, data) {
+				if (err) cbk(false);
+				else    cbk(v);
+			});		
+		}		
 		this.init();
 	};
 	module.exports = obj;
