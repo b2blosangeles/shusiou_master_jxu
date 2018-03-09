6106915d8ac3d3621821bd7806fa08a114bf3417
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 17:01:34 -0800

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -19,8 +19,166 @@
 			
 			var CP = new pkg.crowdProcess();
 			var _f = {}; 
+			/*
+			_f['videoinfo'] = function(cbk) { 
+				pkg.request(me.space_url +  me.space_info, 
+				function (err, res, body) {
+					let v = (err) ? false : {};
+					if (v !== false) { 
+						try {  v = JSON.parse(body); } catch (e) { v = false; }
+					}
+					if (v === false) { 
+						let buff = new Buffer(100);
+						pkg.fs.stat(source_path + source_file, function(err, stat) {
+							pkg.fs.open(source_path + source_file, 'r', function(err, fd) {
+								pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
+									var start = buffer.indexOf(new Buffer('mvhd')) + 17;
+									var timeScale = buffer.readUInt32BE(start, 4);
+									var duration = buffer.readUInt32BE(start + 4, 4);
+									var movieLength = Math.floor(duration/timeScale);
+									var v = {filesize:stat.size,time_scale:timeScale, trunksize: trunkSize,
+										duration: duration, length:movieLength};
+									writeInfo(v, function() {
+										cbk(v);
+									});
+								});
+							});
+						});		
+					} else {
+						cbk(v);
+					}
+				});		
+			};
+
+			_f['tracks'] = function(cbk) {
+				if (CP.data.videoinfo === false) {
+					cbk('no videoinfo');
+				} else {
+					var folderP = require(env.site_path + '/api/inc/folderP/folderP');
+					var fp = new folderP();		
+					fp.build(tmp_folder, () => {
+						pkg.fs.readdir( tmp_folder, (err, files) => {
+							if (_type === '_t')
+								var condition = (files.length != Math.ceil(CP.data.videoinfo.filesize / trunkSize));
+							else if (_type === '_s')
+								var condition = (files.length != Math.ceil(CP.data.videoinfo.length / 5));
+							else var condition = false;
+
+							if (err || condition) {
+								splitVideo(function(data) { cbk(data); });
+							} else {
+								cbk(files);					
+							}
+						});			
+
+					});
+				}
+			};
+
+			_f['space'] = function(cbk) { 
+				var params = { 
+				  Bucket: space_id,
+				  Delimiter: '',
+				  Prefix: space_dir
+				}, v = {};
+
+				s3.listObjects(params, function (err, data) {
+					if(err)cbk(err.message);
+					else {
+						for (var o in data.Contents) {
+							let key = data.Contents[o].Key.replace(space_dir, '');
+							if (key != '_info.txt') v[key] = data.Contents[o].Size;
+						}
+						cbk(v);
+					}
+				});
+			}
+			_f['clean_space'] = function(cbk) { 
+				let tracks = CP.data.tracks, objs = Object.keys(CP.data.space);
+				let diff = objs.filter(x => !tracks.includes(x));
+				if (diff.length) {
+					CP.exit = 1;
+					removeObjects(space_dir, diff, cbk);
+				} else {
+					cbk(true);
+				}
+			}
+			_f['upload'] = function(cbk) { 
+				let tracks = CP.data.tracks;
+				if (typeof tracks === 'string') {
+					cbk(tracks);
+					CP.exit = 1;
+					return true;
+				} 
+				let objs = CP.data.space;
+				let CP1 = new pkg.crowdProcess(), _f1 = {};
+				let tm = new Date().getTime();
+
+				let uploaded = 0;
+
+				for (var t in tracks) {
+					_f1['P_' + t] = (function(t) { 
+						return function(cbk1) {
+							if (new Date().getTime() - tm > 30000) {
+								cbk1(true); return true;
+							}
+							pkg.fs.stat( tmp_folder + tracks[t], function (err, stat) {
+								if (stat.size !== objs[tracks[t]] || !objs[tracks[t]]) {
+									pkg.fs.readFile( tmp_folder + tracks[t], function (err, data0) {
+									  if (err) { throw err; }
+									     var base64data = new Buffer(data0, 'binary');
+									     var params = {
+										 Body: base64data,
+										 Bucket: space_id,
+										 Key: space_dir + tracks[t],
+										 ContentType: 'video/mp4',
+										 ACL: 'public-read'
+									     };	
+									     s3.putObject(params, function(err, data) {
+										 if (err) cbk1(err.message);
+										 else {
+											 uploaded++;
+											 cbk1(tracks[t])
+										 }	 
+									     });
+									});					
+								} else {
+									cbk1('Skip ' + tracks[t]);
+								}
+							});
+						}
+					})(t);			
+				}
+				CP1.serial(
+					_f1,
+					function(results) {
+						if (!uploaded) {
+							if (Object.keys(CP.data.space).length == CP.data.tracks.length && (CP.data.tracks.length)) {
+								let v = CP.data.videoinfo;
+								v[_type] = tracks; 
+								if (!v['status']) v['status'] = {}; 
+								v['status'][_type] = 1;						
+								writeInfo(v, function() {
+									cbk(true);
+								});
+							} else {
+								cbk(false);
+							}
+
+						}
+					},
+					50000
+				);
+			}	*/
+			CP.serial(
+				_f,
+				function(results) {
+					_cbk('==niu==3=');
+					// _cbk(results);
+				},
+				58000
+			);			
 			
-			_cbk('==niu==2=');
 		};
 		this.init = function() {
 			let me = this;
