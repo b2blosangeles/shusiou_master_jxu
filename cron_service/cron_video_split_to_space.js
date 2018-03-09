d5835580734c4f55a85d8e8e8fe9e4e78aa26910
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:32:37 -0800

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -7,22 +7,15 @@ env.site_path = env.root_path + '/site';
 env.config_path = '/var/qalet_config';
 
 var config = require(env.config_path + '/config.json');
-let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
-    cfg = {
-      id:'shusiou-d-01',
-      endpoint : 'nyc3.digitaloceanspaces.com',
-      accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
-      secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
-    };
 
 let pkg = {
-    mysql:require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
-    crowdProcess:require(env.root_path + '/package/crowdProcess/crowdProcess'),
-	fs : require('fs')
-};  
+    	mysql		: require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
+    	crowdProcess	: require(env.root_path + '/package/crowdProcess/crowdProcess'),
+	fs 		: require('fs')
+}; 
 
-var splitVideo = new awsS3Video(cfg, env, pkg);	
-	
+let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js');
+var splitVideo = new awsS3Video(config, env, pkg);		
 splitVideo.split('_t', '/var/img/video.mp4', 
       function(data) {
         console.log(data);
