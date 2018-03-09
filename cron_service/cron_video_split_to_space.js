2e498e5464d4a31ec553399aecd25076ea5c74e7
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:57:47 -0800

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -11,6 +11,8 @@ var config = require(env.config_path + '/config.json');
 let pkg = {
     	mysql		: require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
     	crowdProcess	: require(env.root_path + '/package/crowdProcess/crowdProcess'),
+	request		: require(env.root_path + '/package/request/node_modules/request'),
+	exec		: require('child_process').exec,
 	fs 		: require('fs')
 }; 
 
