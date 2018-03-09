f2b805ae3811f7702f9b82e4d5455299f7bee0c7
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:20:35 -0800

    Update cron_video_split_to_space.js

--- cron_service/cron_video_split_to_space.js
+++ cron_service/cron_video_split_to_space.js
@@ -14,7 +14,7 @@ let awsS3Video = require(env.site_path + '/api/inc/awsS3Video/awsS3Video.js'),
       accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
       secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
     };
-var splitVideo = new awsS3Video(cfg);	
+var splitVideo = new awsS3Video(cfg, env);	
 splitVideo.split('_t', '/var/img/video.mp4', 
       function(data) {
         console.log(data);
