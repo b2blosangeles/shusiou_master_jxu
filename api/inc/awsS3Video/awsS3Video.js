dac63dd5754bb8bb39835192d6cb0bf3276d23a0
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 16:36:37 -0800

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -1,5 +1,5 @@
 (function () { 
-	var obj =  function (cfg, env) {
+	var obj =  function (config, env, pkg) {
 		this.split = function(_type, _file, _cbk) {
 			let _p = _file.match(/(.+)\/([^\/]+)$/);
 			let source_path = _p[1] + '/',
@@ -10,12 +10,12 @@
 			    space_info = 'shusiou/' + source_file + '/_info.txt',
 			    space_dir = 'shusiou/' + source_file + '/' + _type + '/',
 			    trunkSize = 1024 * 1024;	
-			
+					
 			const AWS = require(env.site_path + '/api/inc/aws-sdk/node_modules/aws-sdk')
 			const s3 = new AWS.S3({
-			    endpoint: new AWS.Endpoint(cfg.endpoint),
-			    accessKeyId: cfg.accessKeyId,
-			    secretAccessKey: cfg.secretAccessKey
+			    endpoint: new AWS.Endpoint('nyc3.digitaloceanspaces.com'),
+			    accessKeyId: config.objectSpaceDigitalOcean.accessKeyId,
+			    secretAccessKey: config.objectSpaceDigitalOcean.secretAccessKey
 			});
 
 			var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
