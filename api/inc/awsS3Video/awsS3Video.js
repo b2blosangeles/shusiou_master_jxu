f3b12bc6fd2ecf7c3ca8ace4902edfe37b2f9970
b2blosangeles <b2blosangeles@gmail.com>
2018-03-08 17:04:39 -0800

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -19,7 +19,7 @@
 			
 			var CP = new pkg.crowdProcess();
 			var _f = {}; 
-			/*
+
 			_f['videoinfo'] = function(cbk) { 
 				pkg.request(me.space_url +  me.space_info, 
 				function (err, res, body) {
@@ -29,16 +29,26 @@
 					}
 					if (v === false) { 
 						let buff = new Buffer(100);
-						pkg.fs.stat(source_path + source_file, function(err, stat) {
-							pkg.fs.open(source_path + source_file, 'r', function(err, fd) {
+						pkg.fs.stat(me.source_path + me.source_file, function(err, stat) {
+							if (err) {
+								CP.exit = 1;
+								cbk(err.message);
+								return true;
+							}
+							pkg.fs.open(me.source_path + me.source_file, 'r', function(err, fd) {
 								pkg.fs.read(fd, buff, 0, 100, 0, function(err, bytesRead, buffer) {
+									if (err) {
+										CP.exit = 1;
+										cbk(err.message);
+										return true;
+									}									
 									var start = buffer.indexOf(new Buffer('mvhd')) + 17;
 									var timeScale = buffer.readUInt32BE(start, 4);
 									var duration = buffer.readUInt32BE(start + 4, 4);
 									var movieLength = Math.floor(duration/timeScale);
-									var v = {filesize:stat.size,time_scale:timeScale, trunksize: trunkSize,
+									var v = {filesize:stat.size,time_scale:timeScale, trunksize: me.trunkSize,
 										duration: duration, length:movieLength};
-									writeInfo(v, function() {
+									me.writeInfo(v, function() {
 										cbk(v);
 									});
 								});
@@ -49,7 +59,7 @@
 					}
 				});		
 			};
-
+			/*
 			_f['tracks'] = function(cbk) {
 				if (CP.data.videoinfo === false) {
 					cbk('no videoinfo');
@@ -173,8 +183,7 @@
 			CP.serial(
 				_f,
 				function(results) {
-					_cbk('==niu==3=');
-					// _cbk(results);
+					_cbk(results);
 				},
 				58000
 			);			
