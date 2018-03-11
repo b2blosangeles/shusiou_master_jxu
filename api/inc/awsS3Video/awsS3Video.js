7ee907d9cdfd951655c1edf3505c683c11502893
b2blosangeles <b2blosangeles@gmail.com>
2018-03-11 16:31:50 -0700

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -86,13 +86,6 @@
 					if (v !== false) { 
 						try {  v = JSON.parse(body); } catch (e) { v = false; }
 					}
-					/*
-					if (!v || !v.status || !v.status._t) {
-						me.split('_t', _file, cbk);
-					} else if (!v.status._s) {
-						me.split('_s', _file, cbk);
-					*/
-	
 					if (!v || !v.status || !v.status._s || !v.status._t) {
 						let CP_A = new pkg.crowdProcess(), _fA = {};
 						_fA['_s'] = function (cbks) {
