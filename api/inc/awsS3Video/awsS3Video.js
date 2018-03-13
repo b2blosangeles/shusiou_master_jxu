3ea6a2088b53b35c8ce86d592ead5f5a47128bc4
b2blosangeles <b2blosangeles@gmail.com>
2018-03-13 09:52:57 -0700

    Update awsS3Video.js

--- api/inc/awsS3Video/awsS3Video.js
+++ api/inc/awsS3Video/awsS3Video.js
@@ -1,14 +1,17 @@
 (function () { 
 	var obj =  function (config, env, pkg) {
-		this.run = function(vid, video_name, cbk) {
+		this.run = function(space, vid, video_name, cbk) {
 			let me = this,
 			    _p = video_name.match(/(.+)\/([^\/]+)$/);
 				
 			me.source_path = _p[1] + '/';
 			me.source_file = _p[2];
-
+			/*
 			me.space_id = 'shusiou-d-01';
-			me.space_url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/';
+			me.space_url = 'https://shusiou-d-01.nyc3.digitaloceanspaces.com/';			
+			*/
+			me.space_id = space.space_id;
+			me.space_url = space.space_url;
 			me.space_info = 'shusiou/' + me.source_file + '/_info.txt';
 			me.trunkSize = 1024 * 1024;
 			
