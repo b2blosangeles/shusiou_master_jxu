4e1fad2cba709779d827109e9aa384c7e603ebdb
b2blosangeles <b2blosangeles@gmail.com>
2018-03-14 14:29:30 -0700

    Update get_default_ad.js

--- api/ad/get_default_ad.js
+++ api/ad/get_default_ad.js
@@ -4,34 +4,40 @@ var mysql = require(env.site_path + '/api/inc/mysql/node_modules/mysql'),
 
 var CP = new pkg.crowdProcess();
 var _f = {};
-_f['P0'] = function(cbk) {
-	cbk(env);  
-};
-_f['Q0'] = function(cbk) {
+_f['dns_matrix'] = function(cbk) {
+	var str = 'SELECT `node_ip` from `cloud_node` WHERE `score` < 900 ORDER BY `node_ip` ASC ';
+	connection.query(str, function (error, results, fields) {
+		connection.end();
+		if (results.length)  cbk(results.length);
+		else cbk(0);
+	}); 				
+}
+_f['P1'] = function(cbk) {
 	var connection = mysql.createConnection(cfg0);
 	connection.connect();
 
-	var str = 'SELECT A.*, B.`node_ip` FROM  `video` A LEFT JOIN `video_node` B ON A.`vid` = B.`vid` WHERE 1 ORDER BY A.`uploaded` DESC LIMIT 3';
+	var str = 'SELECT A.*, B.`created` AS addtime, C.`status` AS space_status, C.`space` FROM  `video` A ' +
+	    ' LEFT JOIN `video_user` B ON A.`vid` = B.`vid` ' +
+	    ' LEFT JOIN `video_space` AS C ON A.`vid` = C.`vid` ' +
+	    " WHERE space_status = 1 ORDER BY A.`uploaded` DESC LIMIT 3";
 
 	connection.query(str, function (error, results, fields) {
 		connection.end();
-		if (error) {
-			cbk(error.message);
-			return true;
-		} else {
-			cbk(results);
-		}
+		if (results.length)  cbk(results);
+		else cbk([]);
 	});  
 };
+
 
 CP.serial(
 	_f,
 	function(data) {
-		var list = [];
-		for (o in data.results.Q0) {
-			list[list.length] = data.results.Q0[o];
-		}
-		res.send({_spent_time:data._spent_time, status:data.status, data:list, env:env});
+		var d = [];
+		for (var i = 0; i < CP.data.P1.length; i++) {
+			CP.data.P1[i].dns_matrix = CP.data.dns_matrix;
+			d.push(data.results.P1[i]);
+		}		
+		res.send({_spent_time:data._spent_time, status:data.status, data:d});
 	},
 	6000
 );
