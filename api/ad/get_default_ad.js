73fcc1077e48093b1e4a8257b5dd14ac79c3d33c
b2blosangeles <b2blosangeles@gmail.com>
2018-03-14 14:41:04 -0700

    Update get_default_ad.js

--- api/ad/get_default_ad.js
+++ api/ad/get_default_ad.js
@@ -39,7 +39,7 @@ CP.serial(
 		for (var i = 0; i < CP.data.P1.length; i++) {
 			CP.data.P1[i].dns_matrix = CP.data.dns_matrix;
 			d.push(data.results.P1[i]);
-		})/		
+		})*/		
 		res.send({_spent_time:data._spent_time, status:data.status, data:d});
 	},
 	6000
