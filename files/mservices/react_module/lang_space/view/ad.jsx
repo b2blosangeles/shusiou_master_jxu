d95083ecba675084b2082c3d43a5c446eb793c88
b2blosangeles <b2blosangeles@gmail.com>
2018-03-13 17:13:08 -0700

    Update ad.jsx

--- files/mservices/react_module/lang_space/view/ad.jsx
+++ files/mservices/react_module/lang_space/view/ad.jsx
@@ -53,7 +53,7 @@ try {
 			var idx = Math.floor(Math.random()*me.state.adlist.length);
 			//var l = 'http://' + me.state.adlist[idx].node_ip + '/api/video/play_stream.api?type=section&vid='+
 			//    me.state.adlist[idx].vid+'&s=30&l=30&server=' + me.state.adlist[idx].server_ip;
-			var l = 'http://198.199.120.18/api/video/pipe_stream.api?video_fn=' + me.state.adlist[idx].vid +
+			var l = 'http://198.199.120.18/api/video/pipe.api?video_fn=' + me.state.adlist[idx].vid +
 				'&ss=30&t=30';
 			//var l = 'https://nd1.service.shusiou.win/api/video/play_stream.api?type=section&vid='+
 			//    me.state.adlist[idx].vid+'&s=30&l=30&server=' + me.state.adlist[idx].server_ip;			
